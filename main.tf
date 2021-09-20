b369c96e1f31fb13bad52d622580b2a526ae2905
Ayman Bagabas <ayman.bagabas@gmail.com>
2021-09-20 16:36:00 -0400

    Bump tf module to 0.3.2 to enable provisioning throughput

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ variable "authorization_keys" {
 module "soft_serve" {
   # source = "../terraform-aws-soft-serve"
   source  = "app.terraform.io/charm/soft-serve/aws"
-  version = "0.3.1"
+  version = "0.3.2"
 
   environment                  = var.environment
   aws_region                   = var.aws_region
