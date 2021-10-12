a10d676bbf1eb2b6b2ca00a4cf414471234d6eca
Christian Rocha <christian@rocha.is>
2021-10-12 12:44:23 -0400

    Adjust comments in default config and add some newlines

--- internal/config/defaults.go
+++ internal/config/defaults.go
@@ -2,35 +2,44 @@ package config
 
 const defaultReadme = "# Soft Serve\n\n Welcome! You can configure your Soft Serve server by cloning this repo and pushing changes.\n\n```\ngit clone ssh://{{.Host}}:{{.Port}}/config\n```"
 
-const defaultConfig = `name: Soft Serve
+const defaultConfig = `# The name of the server to show in the TUI.
+name: Soft Serve
+
+# The host and port to listen on. Defaults to 0.0.0.0:23231.
 host: %s
 port: %d
 
-# Set the access level for anonymous users. Options are: read-write, read-only and no-access.
+# Access level for anonymous users. Options are: read-write, read-only and
+# no-access.
 anon-access: %s
 
-# Grant read-only access to users without private keys. Any password will be accepted.
+# You can grant read-only access to users without private keys. Any password
+# will be accepted.
 allow-keyless: false
 
-# Customize repo display in the menu.
+# Customize repo display in the menu. Only repos in this list will appear in
+# the TUI.
 repos:
   - name: Home
     repo: config
     private: true
-    note: "Configuration and content repo for this server"`
+    note: "Configuration and content repo for this server"
+`
 
 const hasKeyUserConfig = `
 
-# Users can read all repos and push to collab-repos. Admins have full access to all repos.
+# Authorized users. Admins have full access to all repos. Users can read all
+# repos and push to their collab-repos.
 users:
-  - name: admin
+  - name: Admin
     admin: true
     public-keys:
-%s`
+%s
+`
 
 const defaultUserConfig = `
 # users:
-#   - name: admin
+#   - name: Admin
 #     admin: true
 #     public-keys:
 #       - KEY TEXT`
@@ -40,4 +49,5 @@ const exampleUserConfig = `
 #     collab-repos:
 #       - REPO
 #     public-keys:
-#       - KEY TEXT`
+#       - KEY TEXT
+`
