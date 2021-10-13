b76994d97f293198f94f9f381ca0a8d33a4aec56
Ayman Bagabas <ayman.bagabas@gmail.com>
2021-10-13 11:52:47 -0400

    stats: more informative stats

--- stats/stats.go
+++ stats/stats.go
@@ -1,8 +1,28 @@
 package stats
 
+import "log"
+
 // Stats provides an interface that can be used to collect metrics about the server.
 type Stats interface {
-	Tui()
-	Push()
-	Fetch()
+	Tui(action string)
+	Push(repo string)
+	Fetch(repo string)
+}
+
+type stats struct{}
+
+func (s *stats) Tui(action string) {
+	log.Printf("TUI: %s", action)
+}
+
+func (s *stats) Push(repo string) {
+	log.Printf("git push: %s", repo)
+}
+
+func (s *stats) Fetch(repo string) {
+	log.Printf("git fetch: %s", repo)
+}
+
+func NewStats() Stats {
+	return &stats{}
 }
