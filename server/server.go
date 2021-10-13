bafd57b67e9f0c96d3d7b141c222d3145b8482b6
Ayman Bagabas <ayman.bagabas@gmail.com>
2021-10-13 11:56:08 -0400

    server: move server to its own package

---
+++ server/server.go
@@ -0,0 +1,58 @@
+package server
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/charmbracelet/soft/config"
+	appCfg "github.com/charmbracelet/soft/internal/config"
+	"github.com/charmbracelet/soft/internal/tui"
+
+	"github.com/charmbracelet/wish"
+	bm "github.com/charmbracelet/wish/bubbletea"
+	gm "github.com/charmbracelet/wish/git"
+	lm "github.com/charmbracelet/wish/logging"
+	"github.com/gliderlabs/ssh"
+)
+
+type Server struct {
+	SSHServer *ssh.Server
+	Config    *config.Config
+	AppConfig *appCfg.Config
+}
+
+// NewServer returns a new *ssh.Server configured to serve Soft Serve. The SSH
+// server key-pair will be created if none exists. An initial admin SSH public
+// key can be provided with authKey. If authKey is provided, access will be
+// restricted to that key. If authKey is not provided, the server will be
+// publicly writable until configured otherwise by cloning the `config` repo.
+func NewServer(cfg *config.Config) *Server {
+	ac, err := appCfg.NewConfig(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	mw := []wish.Middleware{
+		bm.Middleware(tui.SessionHandler(ac)),
+		gm.Middleware(cfg.RepoPath, ac),
+		lm.Middleware(),
+	}
+	s, err := wish.NewServer(
+		ssh.PublicKeyAuth(ac.PublicKeyHandler),
+		ssh.PasswordAuth(ac.PasswordHandler),
+		wish.WithAddress(fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)),
+		wish.WithHostKeyPath(cfg.KeyPath),
+		wish.WithMiddleware(mw...),
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return &Server{
+		SSHServer: s,
+		Config:    cfg,
+		AppConfig: ac,
+	}
+}
+
+func (srv *Server) Start() error {
+	return srv.SSHServer.ListenAndServe()
+}
