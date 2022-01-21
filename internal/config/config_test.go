3264f7de68f7578750c81587038b87de82954de9
Carlos A Becker <caarlos0@gmail.com>
2022-01-21 14:31:12 -0300

    fix: no initial keys
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/matryer/is"
 )
 
-func TestConfigInitialKeys(t *testing.T) {
+func TestMultipleInitialKeys(t *testing.T) {
 	cfg, err := NewConfig(&config.Config{
 		RepoPath: t.TempDir(),
 		KeyPath:  t.TempDir(),
@@ -21,5 +21,15 @@ func TestConfigInitialKeys(t *testing.T) {
 	is.Equal(cfg.Users[0].PublicKeys, []string{
 		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINMwLvyV3ouVrTysUYGoJdl5Vgn5BACKov+n9PlzfPwH a@b",
 		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFxIobhwtfdwN7m1TFt9wx3PsfvcAkISGPxmbmbauST8 a@b",
+	}) // should have both keys
+}
+
+func TestEmptyInitialKeys(t *testing.T) {
+	cfg, err := NewConfig(&config.Config{
+		RepoPath: t.TempDir(),
+		KeyPath:  t.TempDir(),
 	})
+	is := is.New(t)
+	is.NoErr(err)
+	is.Equal(len(cfg.Users), 0) // should not have any users
 }
