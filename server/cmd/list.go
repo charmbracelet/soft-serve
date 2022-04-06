0aec8c2ed4b8d1a9a416128cdae72995bd2ea006
Ayman Bagabas <ayman.bagabas@gmail.com>
2022-04-05 22:10:00 -0400

    feat: add soft-serve middleware commands
    * list files
    * cat files
    * reload config
    * git command

---
+++ server/cmd/list.go
@@ -0,0 +1,81 @@
+package cmd
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"github.com/charmbracelet/soft-serve/git"
+	gitwish "github.com/charmbracelet/wish/git"
+	"github.com/spf13/cobra"
+)
+
+// ListCommand returns a command that list file or directory at path.
+func ListCommand() *cobra.Command {
+	lsCmd := &cobra.Command{
+		Use:     "ls PATH",
+		Aliases: []string{"list"},
+		Short:   "List file or directory at path.",
+		Args:    cobra.RangeArgs(0, 1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ac, s := fromContext(cmd)
+			rn := ""
+			path := ""
+			ps := []string{}
+			if len(args) > 0 {
+				path = filepath.Clean(args[0])
+				ps = strings.Split(path, "/")
+				rn = ps[0]
+				auth := ac.AuthRepo(rn, s.PublicKey())
+				if auth < gitwish.ReadOnlyAccess {
+					return ErrUnauthorized
+				}
+			}
+			if path == "" || path == "." || path == "/" {
+				for _, r := range ac.Source.AllRepos() {
+					fmt.Fprintln(s, r.Name())
+				}
+				return nil
+			}
+			r, err := ac.Source.GetRepo(rn)
+			if err != nil {
+				return err
+			}
+			head, err := r.HEAD()
+			if err != nil {
+				return err
+			}
+			tree, err := r.Tree(head, "")
+			if err != nil {
+				return err
+			}
+			subpath := strings.Join(ps[1:], "/")
+			ents := git.Entries{}
+			te, err := tree.TreeEntry(subpath)
+			if err == git.ErrRevisionNotExist {
+				return ErrFileNotFound
+			}
+			if err != nil {
+				return err
+			}
+			if te.Type() == "tree" {
+				tree, err = tree.SubTree(subpath)
+				if err != nil {
+					return err
+				}
+				ents, err = tree.Entries()
+				if err != nil {
+					return err
+				}
+			} else {
+				ents = append(ents, te)
+			}
+			ents.Sort()
+			for _, ent := range ents {
+				fmt.Fprintf(s, "%s\t%d\t %s\n", ent.Mode(), ent.Size(), ent.Name())
+			}
+			return nil
+		},
+	}
+	return lsCmd
+}
--- server/middleware.go
+++ server/middleware.go
@@ -1,186 +1,48 @@
 package server
 
 import (
+	"context"
 	"fmt"
-	"path/filepath"
-	"strings"
 
-	"github.com/alecthomas/chroma/lexers"
-	gansi "github.com/charmbracelet/glamour/ansi"
-	"github.com/charmbracelet/lipgloss"
-	"github.com/charmbracelet/soft-serve/git"
 	appCfg "github.com/charmbracelet/soft-serve/internal/config"
-	"github.com/charmbracelet/soft-serve/tui/common"
+	"github.com/charmbracelet/soft-serve/server/cmd"
 	"github.com/charmbracelet/wish"
-	gitwish "github.com/charmbracelet/wish/git"
 	"github.com/gliderlabs/ssh"
-	ggit "github.com/gogs/git-module"
-	"github.com/muesli/termenv"
 )
 
-var (
-	lineDigitStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("239"))
-	lineBarStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("236"))
-	dirnameStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("#00AAFF"))
-	filenameStyle  = lipgloss.NewStyle()
-	filemodeStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("#777777"))
-)
-
-// softServeMiddleware is a middleware that handles displaying files with the
-// option of syntax highlighting and line numbers.
-func softServeMiddleware(ac *appCfg.Config) wish.Middleware {
+// softMiddleware is the Soft Serve middleware that handles SSH commands.
+func softMiddleware(ac *appCfg.Config) wish.Middleware {
 	return func(sh ssh.Handler) ssh.Handler {
 		return func(s ssh.Session) {
-			_, _, active := s.Pty()
-			cmds := s.Command()
-			if !active && len(cmds) > 0 {
-				func() {
-					color := false
-					lineno := false
-					fp := filepath.Clean(cmds[0])
-					ps := strings.Split(fp, "/")
-					repo := ps[0]
-					if repo == "config" {
-						return
-					}
-					repoExists := false
-					for _, rp := range ac.Source.AllRepos() {
-						if rp.Name() == repo {
-							repoExists = true
-							break
-						}
-					}
-					if !repoExists {
-						s.Write([]byte("repository not found"))
-						s.Exit(1)
-						return
-					}
-					auth := ac.AuthRepo(repo, s.PublicKey())
-					if auth < gitwish.ReadOnlyAccess {
-						s.Write([]byte("unauthorized"))
-						s.Exit(1)
-						return
-					}
-					for _, op := range cmds[1:] {
-						if op == "-c" || op == "--color" {
-							color = true
-						} else if op == "-l" || op == "--lineno" || op == "--linenumber" {
-							lineno = true
-						}
-					}
-					rs, err := ac.Source.GetRepo(repo)
-					if err != nil {
-						_, _ = s.Write([]byte(err.Error()))
-						_ = s.Exit(1)
-						return
-					}
-					ref, err := rs.HEAD()
-					if err != nil {
-						_, _ = s.Write([]byte(err.Error()))
-						_ = s.Exit(1)
-						return
-					}
-					p := strings.Join(ps[1:], "/")
-					t, err := rs.Tree(ref, p)
-					if err != nil && err != ggit.ErrRevisionNotExist {
-						_, _ = s.Write([]byte(err.Error()))
-						_ = s.Exit(1)
-						return
-					}
-					if err == ggit.ErrRevisionNotExist {
-						_, _ = s.Write([]byte(git.ErrFileNotFound.Error()))
-						_ = s.Exit(1)
-						return
-					}
-					ents, err := t.Entries()
-					if err != nil {
-						fc, _, err := rs.LatestFile(p)
-						if err != nil {
-							_, _ = s.Write([]byte(err.Error()))
-							_ = s.Exit(1)
-							return
-						}
-						if color {
-							ffc, err := withFormatting(fp, fc)
-							if err != nil {
-								s.Write([]byte(err.Error()))
-								s.Exit(1)
-								return
-							}
-							fc = ffc
-						}
-						if lineno {
-							fc = withLineNumber(fc, color)
-						}
-						s.Write([]byte(fc))
-					} else {
-						ents.Sort()
-						for _, e := range ents {
-							m := e.Mode()
-							if m == 0 {
-								s.Write([]byte(strings.Repeat(" ", 10)))
-							} else {
-								s.Write([]byte(filemodeStyle.Render(m.String())))
-							}
-							s.Write([]byte(" "))
-							if !e.IsTree() {
-								s.Write([]byte(filenameStyle.Render(e.Name())))
-							} else {
-								s.Write([]byte(dirnameStyle.Render(e.Name())))
-							}
-							s.Write([]byte("\n"))
-						}
-					}
-				}()
-			}
+			func() {
+				_, _, active := s.Pty()
+				if active {
+					return
+				}
+				ctx := s.Context()
+				ctx = context.WithValue(ctx, "config", ac) //nolint:revive
+				ctx = context.WithValue(ctx, "session", s) //nolint:revive
+
+				use := "ssh"
+				port := ac.Cfg.Port
+				if port != 22 {
+					use += fmt.Sprintf(" -p%d", port)
+				}
+				use += fmt.Sprintf(" %s", ac.Cfg.Host)
+				cmd := cmd.RootCommand()
+				cmd.Use = use
+				cmd.SetIn(s)
+				cmd.SetOut(s)
+				cmd.SetErr(s)
+				cmd.SetArgs(s.Command())
+				err := cmd.ExecuteContext(ctx)
+				if err != nil {
+					_, _ = s.Write([]byte(err.Error()))
+					_ = s.Exit(1)
+					return
+				}
+			}()
 			sh(s)
 		}
 	}
-}
-
-func withLineNumber(s string, color bool) string {
-	lines := strings.Split(s, "\n")
-	// NB: len() is not a particularly safe way to count string width (because
-	// it's counting bytes instead of runes) but in this case it's okay
-	// because we're only dealing with digits, which are one byte each.
-	mll := len(fmt.Sprintf("%d", len(lines)))
-	for i, l := range lines {
-		digit := fmt.Sprintf("%*d", mll, i+1)
-		bar := "│"
-		if color {
-			digit = lineDigitStyle.Render(digit)
-			bar = lineBarStyle.Render(bar)
-		}
-		if i < len(lines)-1 || len(l) != 0 {
-			// If the final line was a newline we'll get an empty string for
-			// the final line, so drop the newline altogether.
-			lines[i] = fmt.Sprintf(" %s %s %s", digit, bar, l)
-		}
-	}
-	return strings.Join(lines, "\n")
-}
-
-func withFormatting(p, c string) (string, error) {
-	zero := uint(0)
-	lang := ""
-	lexer := lexers.Match(p)
-	if lexer != nil && lexer.Config() != nil {
-		lang = lexer.Config().Name
-	}
-	formatter := &gansi.CodeBlockElement{
-		Code:     c,
-		Language: lang,
-	}
-	r := strings.Builder{}
-	styles := common.DefaultStyles()
-	styles.CodeBlock.Margin = &zero
-	rctx := gansi.NewRenderContext(gansi.Options{
-		Styles:       styles,
-		ColorProfile: termenv.TrueColor,
-	})
-	err := formatter.Render(&r, rctx)
-	if err != nil {
-		return "", err
-	}
-	return r.String(), nil
 }
