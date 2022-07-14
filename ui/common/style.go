6271ec898feaba7ea867279f87ded18f3dc0040c
Christian Rocha <christian@rocha.is>
2022-07-13 17:24:32 -0700

    chore: de-emphasize Glamour headers

--- ui/common/style.go
+++ ui/common/style.go
@@ -5,15 +5,23 @@ import (
 	gansi "github.com/charmbracelet/glamour/ansi"
 )
 
+func strptr(s string) *string {
+	return &s
+}
+
 // StyleConfig returns the default Glamour style configuration.
 func StyleConfig() gansi.StyleConfig {
-	noColor := ""
+	noColor := strptr("")
 	s := glamour.DarkStyleConfig
-	s.Document.StylePrimitive.Color = &noColor
-	s.CodeBlock.Chroma.Text.Color = &noColor
-	s.CodeBlock.Chroma.Name.Color = &noColor
+	s.H1.BackgroundColor = noColor
+	s.H1.Prefix = "# "
+	s.H1.Suffix = ""
+	s.H1.Color = strptr("39")
+	s.Document.StylePrimitive.Color = noColor
+	s.CodeBlock.Chroma.Text.Color = noColor
+	s.CodeBlock.Chroma.Name.Color = noColor
 	// This fixes an issue with the default style config. For example
 	// highlighting empty spaces with red in Dockerfile type.
-	s.CodeBlock.Chroma.Error.BackgroundColor = &noColor
+	s.CodeBlock.Chroma.Error.BackgroundColor = noColor
 	return s
 }
