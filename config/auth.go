d88ccb97d3e756be489d680d29e7dc24b9792fb3
Ayman Bagabas <ayman.bagabas@gmail.com>
2022-10-18 15:16:19 -0400

    ref(config): clarify repo auth for key
    Fix edge cases when anonAccess is greater than collab

--- config/auth.go
+++ config/auth.go
@@ -59,33 +59,7 @@ func (cfg *Config) PublicKeyHandler(ctx ssh.Context, pk ssh.PublicKey) bool {
 	return cfg.accessForKey("", pk) != gm.NoAccess
 }
 
-func (cfg *Config) accessForKey(repo string, pk ssh.PublicKey) gm.AccessLevel {
-	private := cfg.isPrivate(repo)
-	for _, u := range cfg.Users {
-		for _, k := range u.PublicKeys {
-			apk, _, _, _, err := ssh.ParseAuthorizedKey([]byte(strings.TrimSpace(k)))
-			if err != nil {
-				log.Printf("error: malformed authorized key: '%s'", k)
-				return gm.NoAccess
-			}
-			if ssh.KeysEqual(pk, apk) {
-				if u.Admin {
-					return gm.AdminAccess
-				}
-				for _, r := range u.CollabRepos {
-					if repo == r {
-						return gm.ReadWriteAccess
-					}
-				}
-				if !private {
-					return gm.ReadOnlyAccess
-				}
-			}
-		}
-	}
-	if private && len(cfg.Users) > 0 {
-		return gm.NoAccess
-	}
+func (cfg *Config) anonAccessLevel() gm.AccessLevel {
 	switch cfg.AnonAccess {
 	case "no-access":
 		return gm.NoAccess
@@ -98,4 +72,66 @@ func (cfg *Config) accessForKey(repo string, pk ssh.PublicKey) gm.AccessLevel {
 	default:
 		return gm.NoAccess
 	}
+}
+
+// accessForKey returns the access level for the given repo.
+//
+// If repo doesn't exist, then access is based on user's admin privileges, or
+// config.AnonAccess.
+// If repo exists, and private, then admins and collabs are allowed access.
+// If repo exists, and not private, then access is based on config.AnonAccess.
+func (cfg *Config) accessForKey(repo string, pk ssh.PublicKey) gm.AccessLevel {
+	var u *User
+	var r *RepoConfig
+	anon := cfg.anonAccessLevel()
+OUT:
+	// Find user
+	for _, user := range cfg.Users {
+		for _, k := range user.PublicKeys {
+			apk, _, _, _, err := ssh.ParseAuthorizedKey([]byte(strings.TrimSpace(k)))
+			if err != nil {
+				log.Printf("error: malformed authorized key: '%s'", k)
+				return gm.NoAccess
+			}
+			if ssh.KeysEqual(pk, apk) {
+				us := user
+				u = &us
+				break OUT
+			}
+		}
+	}
+	// Find repo
+	for _, rp := range cfg.Repos {
+		if rp.Repo == repo {
+			rr := rp
+			r = &rr
+			break
+		}
+	}
+	if u != nil && u.Admin {
+		return gm.AdminAccess
+	}
+	if r == nil || len(cfg.Users) == 0 {
+		return anon
+	}
+	// Collabs default access is read-write
+	if u != nil {
+		ac := gm.ReadWriteAccess
+		if anon > ac {
+			ac = anon
+		}
+		for _, rr := range u.CollabRepos {
+			if rr == r.Repo {
+				return ac
+			}
+		}
+	}
+	// Users default access is read-only
+	if !r.Private {
+		if anon > gm.ReadOnlyAccess {
+			return anon
+		}
+		return gm.ReadOnlyAccess
+	}
+	return gm.NoAccess
 }
