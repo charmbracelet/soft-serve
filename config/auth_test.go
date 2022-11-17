charmbracelet/soft-serve

commit d88ccb97d3e756be489d680d29e7dc24b9792fb3

ref(config): clarify repo auth for key
Fix edge cases when anonAccess is greater than collab

config/auth.go
@@ -59,33 +59,7 @@ func (cfg *Config) PublicKeyHandler(ctx ssh.Context, pk ssh.PublicKey) bool {
 	return cfg.accessForKey("", pk) != gm.NoAccess
 }
 
-func (cfg *Config) accessForKey(repo string, pk ssh.PublicKey) gm.AccessLevel {
-	private := cfg.isPrivate(repo)
-	for _, u := range cfg.Users {
-		for _, k := range u.PublicKeys {
-			apk, _, _, _, err := ssh.ParseAuthorizedKey([]byte(strings.TrimSpace(k)))
-			if err != nil {
-				log.Printf("error: malformed authorized key: '%s'", k)
-				return gm.NoAccess
-			}
-			if ssh.KeysEqual(pk, apk) {
-				if u.Admin {
-					return gm.AdminAccess
-				}
-				for _, r := range u.CollabRepos {
-					if repo == r {
-						return gm.ReadWriteAccess
-					}
-				}
-				if !private {
-					return gm.ReadOnlyAccess
-				}
-			}
-		}
-	}
-	if private && len(cfg.Users) > 0 {
-		return gm.NoAccess
-	}
+func (cfg *Config) anonAccessLevel() gm.AccessLevel {
 	switch cfg.AnonAccess {
 	case "no-access":
 		return gm.NoAccess
@@ -98,4 +72,66 @@ func (cfg *Config) accessForKey(repo string, pk ssh.PublicKey) gm.AccessLevel {
 	default:
 		return gm.NoAccess
 	}
+}
+
+// accessForKey returns the access level for the given repo.
+//
+// If repo doesn't exist, then access is based on user's admin privileges, or
+// config.AnonAccess.
+// If repo exists, and private, then admins and collabs are allowed access.
+// If repo exists, and not private, then access is based on config.AnonAccess.
+func (cfg *Config) accessForKey(repo string, pk ssh.PublicKey) gm.AccessLevel {
+	var u *User
+	var r *RepoConfig
+	anon := cfg.anonAccessLevel()
+OUT:
+	// Find user
+	for _, user := range cfg.Users {
+		for _, k := range user.PublicKeys {
+			apk, _, _, _, err := ssh.ParseAuthorizedKey([]byte(strings.TrimSpace(k)))
+			if err != nil {
+				log.Printf("error: malformed authorized key: '%s'", k)
+				return gm.NoAccess
+			}
+			if ssh.KeysEqual(pk, apk) {
+				us := user
+				u = &us
+				break OUT
+			}
+		}
+	}
+	// Find repo
+	for _, rp := range cfg.Repos {
+		if rp.Repo == repo {
+			rr := rp
+			r = &rr
+			break
+		}
+	}
+	if u != nil && u.Admin {
+		return gm.AdminAccess
+	}
+	if r == nil || len(cfg.Users) == 0 {
+		return anon
+	}
+	// Collabs default access is read-write
+	if u != nil {
+		ac := gm.ReadWriteAccess
+		if anon > ac {
+			ac = anon
+		}
+		for _, rr := range u.CollabRepos {
+			if rr == r.Repo {
+				return ac
+			}
+		}
+	}
+	// Users default access is read-only
+	if !r.Private {
+		if anon > gm.ReadOnlyAccess {
+			return anon
+		}
+		return gm.ReadOnlyAccess
+	}
+	return gm.NoAccess
 }

config/config.go
@@ -27,11 +27,6 @@ import (
 	"github.com/go-git/go-git/v5/storage/memory"
 )
 
-var (
-	// ErrNoConfig is returned when no config file is found.
-	ErrNoConfig = errors.New("no config file found")
-)
-
 // Config is the Soft Serve configuration.
 type Config struct {
 	Name         string         `yaml:"name" json:"name"`
@@ -40,7 +35,7 @@ type Config struct {
 	AnonAccess   string         `yaml:"anon-access" json:"anon-access"`
 	AllowKeyless bool           `yaml:"allow-keyless" json:"allow-keyless"`
 	Users        []User         `yaml:"users" json:"users"`
-	Repos        []MenuRepo     `yaml:"repos" json:"repos"`
+	Repos        []RepoConfig   `yaml:"repos" json:"repos"`
 	Source       *RepoSource    `yaml:"-" json:"-"`
 	Cfg          *config.Config `yaml:"-" json:"-"`
 	mtx          sync.Mutex
@@ -54,8 +49,8 @@ type User struct {
 	CollabRepos []string `yaml:"collab-repos" json:"collab-repos"`
 }
 
-// Repo contains repository configuration information.
-type MenuRepo struct {
+// RepoConfig is a repository configuration.
+type RepoConfig struct {
 	Name    string `yaml:"name" json:"name"`
 	Repo    string `yaml:"repo" json:"repo"`
 	Note    string `yaml:"note" json:"note"`
@@ -128,6 +123,35 @@ func NewConfig(cfg *config.Config) (*Config, error) {
 	return c, nil
 }
 
+func (cfg *Config) readConfig(repo string, v interface{}) error {
+	cr, err := cfg.Source.GetRepo(repo)
+	if err != nil {
+		return err
+	}
+	cy, _, err := cr.LatestFile(repo + ".yaml")
+	if err != nil && !errors.Is(err, git.ErrFileNotFound) {
+		return fmt.Errorf("error reading %s.yaml: %w", repo, err)
+	}
+	cj, _, err := cr.LatestFile(repo + ".json")
+	if err != nil && !errors.Is(err, git.ErrFileNotFound) {
+		return fmt.Errorf("error reading %s.json: %w", repo, err)
+	}
+	if cy != "" {
+		err = yaml.Unmarshal([]byte(cy), v)
+		if err != nil {
+			return fmt.Errorf("bad yaml in %s.yaml: %s", repo, err)
+		}
+	} else if cj != "" {
+		err = json.Unmarshal([]byte(cj), v)
+		if err != nil {
+			return fmt.Errorf("bad json in %s.json: %s", repo, err)
+		}
+	} else {
+		return fmt.Errorf("no config file found for %q", repo)
+	}
+	return nil
+}
+
 // Reload reloads the configuration.
 func (cfg *Config) Reload() error {
 	cfg.mtx.Lock()
@@ -136,30 +160,8 @@ func (cfg *Config) Reload() error {
 	if err != nil {
 		return err
 	}
-	cr, err := cfg.Source.GetRepo("config")
-	if err != nil {
-		return err
-	}
-	cy, _, err := cr.LatestFile("config.yaml")
-	if err != nil && !errors.Is(err, git.ErrFileNotFound) {
-		return fmt.Errorf("error reading config.yaml: %w", err)
-	}
-	cj, _, err := cr.LatestFile("config.json")
-	if err != nil && !errors.Is(err, git.ErrFileNotFound) {
-		return fmt.Errorf("error reading config.json: %w", err)
-	}
-	if cy != "" {
-		err = yaml.Unmarshal([]byte(cy), cfg)
-		if err != nil {
-			return fmt.Errorf("bad yaml in config.yaml: %s", err)
-		}
-	} else if cj != "" {
-		err = json.Unmarshal([]byte(cj), cfg)
-		if err != nil {
-			return fmt.Errorf("bad json in config.json: %s", err)
-		}
-	} else {
-		return ErrNoConfig
+	if err := cfg.readConfig("config", cfg); err != nil {
+		return fmt.Errorf("error reading config: %w", err)
 	}
 	for _, r := range cfg.Source.AllRepos() {
 		name := r.Name()
@@ -274,15 +276,6 @@ func (cfg *Config) createDefaultConfigRepo(yaml string) error {
 		return err
 	}
 	return cfg.Reload()
-}
-
-func (cfg *Config) isPrivate(repo string) bool {
-	for _, r := range cfg.Repos {
-		if r.Repo == repo {
-			return r.Private
-		}
-	}
-	return false
 }
 
 func templatize(mdt string, tmpl interface{}) (string, error) {

commit 7a7e73dda8cf32681cc31bedf64055f0094a2e21

fix(auth): wrong auth for anon users
This fixes an issue with using anon-access with registered users.

Fixes: d88ccb97d3e7 ("ref(config): clarify repo auth for key")

config/auth.go
@@ -81,10 +81,8 @@ func (cfg *Config) anonAccessLevel() gm.AccessLevel {
 // If repo exists, and private, then admins and collabs are allowed access.
 // If repo exists, and not private, then access is based on config.AnonAccess.
 func (cfg *Config) accessForKey(repo string, pk ssh.PublicKey) gm.AccessLevel {
-	var u *User
-	var r *RepoConfig
 	anon := cfg.anonAccessLevel()
-OUT:
+	private := cfg.isPrivate(repo)
 	// Find user
 	for _, user := range cfg.Users {
 		for _, k := range user.PublicKeys {
@@ -94,49 +92,63 @@ OUT:
 				return gm.NoAccess
 			}
 			if ssh.KeysEqual(pk, apk) {
-				us := user
-				u = &us
-				break OUT
+				if user.Admin {
+					return gm.AdminAccess
+				}
+				u := user
+				if cfg.isCollab(repo, &u) {
+					if anon > gm.ReadWriteAccess {
+						return anon
+					}
+					return gm.ReadWriteAccess
+				}
+				if !private {
+					if anon > gm.ReadOnlyAccess {
+						return anon
+					}
+					return gm.ReadOnlyAccess
+				}
 			}
 		}
 	}
-	// Find repo
-	for _, rp := range cfg.Repos {
-		if rp.Repo == repo {
-			rr := rp
-			r = &rr
-			break
+	// Don't restrict access to private repos if no users are configured.
+	// Return anon access level.
+	if private && len(cfg.Users) > 0 {
+		return gm.NoAccess
+	}
+	return anon
+}
+
+func (cfg *Config) findRepo(repo string) *RepoConfig {
+	for _, r := range cfg.Repos {
+		if r.Repo == repo {
+			return &r
 		}
 	}
-	if u != nil && u.Admin {
-		return gm.AdminAccess
+	return nil
+}
+
+func (cfg *Config) isPrivate(repo string) bool {
+	if r := cfg.findRepo(repo); r != nil {
+		return r.Private
 	}
-	if r == nil || len(cfg.Users) == 0 {
-		return anon
-	}
-	// Collabs default access is read-write
-	if u != nil {
-		ac := gm.ReadWriteAccess
-		if anon > ac {
-			ac = anon
-		}
-		for _, c := range r.Collabs {
-			if c == u.Name {
-				return ac
+	return false
+}
+
+func (cfg *Config) isCollab(repo string, user *User) bool {
+	if user != nil {
+		for _, r := range user.CollabRepos {
+			if r == repo {
+				return true
 			}
 		}
-		for _, rr := range u.CollabRepos {
-			if rr == r.Repo {
-				return ac
+		if r := cfg.findRepo(repo); r != nil {
+			for _, c := range r.Collabs {
+				if c == user.Name {
+					return true
+				}
 			}
 		}
 	}
-	// Users default access is read-only
-	if !r.Private {
-		if anon > gm.ReadOnlyAccess {
-			return anon
-		}
-		return gm.ReadOnlyAccess
-	}
-	return gm.NoAccess
+	return false
 }

config/auth_test.go
@@ -35,6 +35,27 @@ func TestAuth(t *testing.T) {
 			},
 		},
 		{
+			name:   "anon access: no-access, anonymous user with admin user",
+			access: git.NoAccess,
+			repo:   "foo",
+			cfg: Config{
+				AnonAccess: "no-access",
+				Repos: []RepoConfig{
+					{
+						Repo: "foo",
+					},
+				},
+				Users: []User{
+					{
+						Admin: true,
+						PublicKeys: []string{
+							adminKey,
+						},
+					},
+				},
+			},
+		},
+		{
 			name:   "anon access: no-access, authd user",
 			key:    dummyPk,
 			repo:   "foo",
@@ -50,6 +71,28 @@ func TestAuth(t *testing.T) {
 					{
 						PublicKeys: []string{
 							dummyKey,
+						},
+					},
+				},
+			},
+		},
+		{
+			name:   "anon access: no-access, anonymous user with admin user",
+			key:    dummyPk,
+			repo:   "foo",
+			access: git.NoAccess,
+			cfg: Config{
+				AnonAccess: "no-access",
+				Repos: []RepoConfig{
+					{
+						Repo: "foo",
+					},
+				},
+				Users: []User{
+					{
+						Admin: true,
+						PublicKeys: []string{
+							adminKey,
 						},
 					},
 				},
@@ -429,13 +472,29 @@ func TestAuth(t *testing.T) {
 			name:   "anon access: no-access, authd user, new repo",
 			key:    dummyPk,
 			repo:   "foo",
-			access: git.NoAccess,
+			access: git.ReadOnlyAccess,
 			cfg: Config{
 				AnonAccess: "no-access",
 				Users: []User{
 					{
 						PublicKeys: []string{
 							dummyKey,
+						},
+					},
+				},
+			},
+		},
+		{
+			name:   "anon access: no-access, authd user, new repo, with user",
+			key:    dummyPk,
+			repo:   "foo",
+			access: git.NoAccess,
+			cfg: Config{
+				AnonAccess: "no-access",
+				Users: []User{
+					{
+						PublicKeys: []string{
+							adminKey,
 						},
 					},
 				},