1d675f3ba7934f336e262de151d0b950e0974482
Ayman Bagabas <ayman.bagabas@gmail.com>
2023-03-21 18:17:53 -0400

    feat(backend): server backend
    Add file backend that uses filesystem to manage non-git operations.

---
+++ server/backend/backend.go
@@ -0,0 +1,84 @@
+package backend
+
+import (
+	"bytes"
+
+	"golang.org/x/crypto/ssh"
+)
+
+// Backend is an interface that handles repositories management and any
+// non-Git related operations.
+type Backend interface {
+	// ServerName returns the server's name.
+	ServerName() string
+	// SetServerName sets the server's name.
+	SetServerName(name string) error
+	// ServerHost returns the server's host.
+	ServerHost() string
+	// SetServerHost sets the server's host.
+	SetServerHost(host string) error
+	// ServerPort returns the server's port.
+	ServerPort() string
+	// SetServerPort sets the server's port.
+	SetServerPort(port string) error
+
+	// AnonAccess returns the access level for anonymous users.
+	AnonAccess() AccessLevel
+	// SetAnonAccess sets the access level for anonymous users.
+	SetAnonAccess(level AccessLevel) error
+	// AllowKeyless returns true if keyless access is allowed.
+	AllowKeyless() bool
+	// SetAllowKeyless sets whether or not keyless access is allowed.
+	SetAllowKeyless(allow bool) error
+
+	// Repository finds the given repository.
+	Repository(repo string) (Repository, error)
+	// Repositories returns a list of all repositories.
+	Repositories() ([]Repository, error)
+	// CreateRepository creates a new repository.
+	CreateRepository(name string, private bool) (Repository, error)
+	// DeleteRepository deletes a repository.
+	DeleteRepository(name string) error
+	// RenameRepository renames a repository.
+	RenameRepository(oldName, newName string) error
+	// DefaultBranch returns the repository's default branch.
+	DefaultBranch(repo string) (string, error)
+	// SetDefaultBranch sets the default branch for a repository.
+	SetDefaultBranch(repo string, branch string) error
+
+	// Description returns the repo's description.
+	Description(repo string) string
+	// SetDescription sets the repo's description.
+	SetDescription(repo, desc string) error
+	// IsPrivate returns true if the repository is private.
+	IsPrivate(repo string) bool
+	// SetPrivate sets the repository's private status.
+	SetPrivate(repo string, priv bool) error
+
+	// IsCollaborator returns true if the authorized key is a collaborator on the repository.
+	IsCollaborator(pk ssh.PublicKey, repo string) bool
+	// AddCollaborator adds the authorized key as a collaborator on the repository.
+	AddCollaborator(pk ssh.PublicKey, repo string) error
+	// IsAdmin returns true if the authorized key is an admin.
+	IsAdmin(pk ssh.PublicKey) bool
+	// AddAdmin adds the authorized key as an admin.
+	AddAdmin(pk ssh.PublicKey) error
+}
+
+// ParseAuthorizedKey parses an authorized key string into a public key.
+func ParseAuthorizedKey(ak string) (ssh.PublicKey, string, error) {
+	pk, c, _, _, err := ssh.ParseAuthorizedKey([]byte(ak))
+	return pk, c, err
+}
+
+// MarshalAuthorizedKey marshals a public key into an authorized key string.
+//
+// This is the inverse of ParseAuthorizedKey.
+// This function is a copy of ssh.MarshalAuthorizedKey, but without the trailing newline.
+// It returns an empty string if pk is nil.
+func MarshalAuthorizedKey(pk ssh.PublicKey) string {
+	if pk == nil {
+		return ""
+	}
+	return string(bytes.TrimSuffix(ssh.MarshalAuthorizedKey(pk), []byte("\n")))
+}
---
+++ server/backend/utils.go
@@ -0,0 +1,51 @@
+package backend
+
+import (
+	"path/filepath"
+
+	"github.com/charmbracelet/soft-serve/git"
+	"github.com/gobwas/glob"
+)
+
+// LatestFile returns the contents of the latest file at the specified path in
+// the repository and its file path.
+func LatestFile(r Repository, pattern string) (string, string, error) {
+	g := glob.MustCompile(pattern)
+	dir := filepath.Dir(pattern)
+	repo, err := r.Repository()
+	if err != nil {
+		return "", "", err
+	}
+	head, err := repo.HEAD()
+	if err != nil {
+		return "", "", err
+	}
+	t, err := repo.TreePath(head, dir)
+	if err != nil {
+		return "", "", err
+	}
+	ents, err := t.Entries()
+	if err != nil {
+		return "", "", err
+	}
+	for _, e := range ents {
+		fp := filepath.Join(dir, e.Name())
+		if e.IsTree() {
+			continue
+		}
+		if g.Match(fp) {
+			bts, err := e.Contents()
+			if err != nil {
+				return "", "", err
+			}
+			return string(bts), fp, nil
+		}
+	}
+	return "", "", git.ErrFileNotFound
+}
+
+// Readme returns the repository's README.
+func Readme(r Repository) (readme string, path string, err error) {
+	readme, path, err = LatestFile(r, "README*")
+	return
+}
