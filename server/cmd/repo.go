ce7b080c4c3a2af3a5b37ac9664a4b8ed2f4bfc4
Ayman Bagabas <ayman.bagabas@gmail.com>
2023-04-04 12:10:26 -0400

    feat(server): add repo mirror & info commands

--- server/cmd/repo.go
+++ server/cmd/repo.go
@@ -19,11 +19,63 @@ func repoCommand() *cobra.Command {
 		hiddenCommand(),
 		importCommand(),
 		listCommand(),
+		mirrorCommand(),
 		privateCommand(),
 		projectName(),
 		renameCommand(),
 		tagCommand(),
 		treeCommand(),
+	)
+
+	cmd.AddCommand(
+		&cobra.Command{
+			Use:               "info REPOSITORY",
+			Short:             "Get information about a repository",
+			Args:              cobra.ExactArgs(1),
+			PersistentPreRunE: checkIfReadable,
+			RunE: func(cmd *cobra.Command, args []string) error {
+				cfg, _ := fromContext(cmd)
+				rn := args[0]
+				rr, err := cfg.Backend.Repository(rn)
+				if err != nil {
+					return err
+				}
+
+				r, err := rr.Open()
+				if err != nil {
+					return err
+				}
+
+				head, err := r.HEAD()
+				if err != nil {
+					return err
+				}
+
+				branches, _ := r.Branches()
+				tags, _ := r.Tags()
+				cmd.Println("Project Name:", rr.ProjectName())
+				cmd.Println("Repository:", rr.Name())
+				cmd.Println("Description:", rr.Description())
+				cmd.Println("Private:", rr.IsPrivate())
+				cmd.Println("Hidden:", rr.IsHidden())
+				cmd.Println("Mirror:", rr.IsMirror())
+				cmd.Println("Default Branch:", head.Name().Short())
+				if len(branches) > 0 {
+					cmd.Println("Branches:")
+					for _, b := range branches {
+						cmd.Println("  -", b)
+					}
+				}
+				if len(tags) > 0 {
+					cmd.Println("Tags:")
+					for _, t := range tags {
+						cmd.Println("  -", t)
+					}
+				}
+
+				return nil
+			},
+		},
 	)
 
 	return cmd
