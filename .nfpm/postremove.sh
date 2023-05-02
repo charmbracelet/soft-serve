12b410fb5347eb7e52e7ac85faab4fd5859b448e
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-05-02 15:07:14 +0000

    fix: format

--- .nfpm/postremove.sh
+++ .nfpm/postremove.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+if ! command -V systemctl >/dev/null 2>&1; then
+	echo "Not running SystemD, ignoring"
+	exit 0
+fi
+
 systemctl stop soft.service
 systemctl disable soft.service
 systemctl daemon-reload
