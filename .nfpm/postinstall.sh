2624e60ee751ddd86b8d4588e97d774a576bc430
Ayman Bagabas <ayman.bagabas@gmail.com>
2023-05-03 13:42:47 -0400

    fix(nfpm): don't enable and start systemd service by default

--- .nfpm/postinstall.sh
+++ .nfpm/postinstall.sh
@@ -9,9 +9,6 @@ fi
 systemd-sysusers
 systemd-tmpfiles --create
 
-echo "Enabling and starting soft-server.service"
 systemctl daemon-reload
 systemctl unmask soft-serve.service
 systemctl preset soft-serve.service
-systemctl enable soft-serve.service
-systemctl restart soft-serve.service
