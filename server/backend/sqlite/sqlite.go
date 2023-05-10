charmbracelet/soft-serve

commit 6eba2455d03c3ddd009ec04235d6a30fb897bc65

fix(backend): delete repo when importing existing one
Fixes: bb40f89b34de ("fix(server): bound the current context to the underlying operation")

server/backend/sqlite/sqlite.go
@@ -184,6 +184,10 @@ func (d *SqliteBackend) ImportRepository(name string, remote string, opts backen
 	repo := name + ".git"
 	rp := filepath.Join(d.reposPath(), repo)
 
+	if _, err := os.Stat(rp); err == nil || os.IsExist(err) {
+		return nil, ErrRepoExist
+	}
+
 	copts := git.CloneOptions{
 		Bare:    true,
 		Mirror:  opts.Mirror,

server/backend/sqlite/error.go
@@ -14,4 +14,7 @@ var (
 
 	// ErrRepoNotExist is returned when a repository does not exist.
 	ErrRepoNotExist = fmt.Errorf("repository does not exist")
+
+	// ErrRepoExist is returned when a repository already exists.
+	ErrRepoExist = fmt.Errorf("repository already exists")
 )

commit 469a86ffdec95416953d7617b85557743b2ae729

fix(backend): delete non-existing repo
Fixes: 6eba2455d03c ("fix(backend): delete repo when importing existing one")

server/backend/sqlite/sqlite.go
@@ -223,18 +223,14 @@ func (d *SqliteBackend) DeleteRepository(name string) error {
 	name = utils.SanitizeRepo(name)
 	repo := name + ".git"
 	rp := filepath.Join(d.reposPath(), repo)
-	if _, err := os.Stat(rp); err != nil {
-		return os.ErrNotExist
-	}
 
-	if err := wrapTx(d.db, d.ctx, func(tx *sqlx.Tx) error {
+	return wrapTx(d.db, d.ctx, func(tx *sqlx.Tx) error {
+		if err := os.RemoveAll(rp); err != nil {
+			return err
+		}
 		_, err := tx.Exec("DELETE FROM repo WHERE name = ?;", name)
 		return err
-	}); err != nil {
-		return wrapDbErr(err)
-	}
-
-	return os.RemoveAll(rp)
+	})
 }
 
 // RenameRepository renames a repository.