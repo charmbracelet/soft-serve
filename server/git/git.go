1413d971dd98d097ff6a8cccd5556ee0c5ef8790
Ayman Bagabas <ayman.bagabas@gmail.com>
2023-05-14 13:45:27 -0400

    refactor: tidy up server git
    use git services to implement handling git server commands
    pass config to git as environment variables

--- server/git/git.go
+++ server/git/git.go
@@ -5,16 +5,12 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 
 	"github.com/charmbracelet/log"
 	"github.com/charmbracelet/soft-serve/git"
-	"github.com/charmbracelet/soft-serve/server/config"
 	"github.com/go-git/go-git/v5/plumbing/format/pktline"
-	"golang.org/x/sync/errgroup"
 )
 
 var (
@@ -37,112 +33,6 @@ var (
 	// ErrTimeout is returned when the maximum read timeout is exceeded.
 	ErrTimeout = errors.New("I/O timeout reached")
 )
-
-// Git protocol commands.
-const (
-	ReceivePackBin   = "git-receive-pack"
-	UploadPackBin    = "git-upload-pack"
-	UploadArchiveBin = "git-upload-archive"
-)
-
-// UploadPack runs the git upload-pack protocol against the provided repo.
-func UploadPack(ctx context.Context, in io.Reader, out io.Writer, er io.Writer, repoDir string, envs ...string) error {
-	exists, err := fileExists(repoDir)
-	if !exists {
-		return ErrInvalidRepo
-	}
-	if err != nil {
-		return err
-	}
-	return RunGit(ctx, in, out, er, "", envs, UploadPackBin[4:], repoDir)
-}
-
-// UploadArchive runs the git upload-archive protocol against the provided repo.
-func UploadArchive(ctx context.Context, in io.Reader, out io.Writer, er io.Writer, repoDir string, envs ...string) error {
-	exists, err := fileExists(repoDir)
-	if !exists {
-		return ErrInvalidRepo
-	}
-	if err != nil {
-		return err
-	}
-	return RunGit(ctx, in, out, er, "", envs, UploadArchiveBin[4:], repoDir)
-}
-
-// ReceivePack runs the git receive-pack protocol against the provided repo.
-func ReceivePack(ctx context.Context, in io.Reader, out io.Writer, er io.Writer, repoDir string, envs ...string) error {
-	if err := RunGit(ctx, in, out, er, "", envs, ReceivePackBin[4:], repoDir); err != nil {
-		return err
-	}
-	return EnsureDefaultBranch(ctx, in, out, er, repoDir)
-}
-
-// RunGit runs a git command in the given repo.
-func RunGit(ctx context.Context, in io.Reader, out io.Writer, er io.Writer, dir string, envs []string, args ...string) error {
-	cfg := config.FromContext(ctx)
-	logger := log.FromContext(ctx).WithPrefix("rungit")
-	c := exec.CommandContext(ctx, "git", args...)
-	c.Dir = dir
-	c.Env = append(os.Environ(), envs...)
-	c.Env = append(c.Env, "PATH="+os.Getenv("PATH"))
-	c.Env = append(c.Env, "SOFT_SERVE_DEBUG="+os.Getenv("SOFT_SERVE_DEBUG"))
-	if cfg != nil {
-		c.Env = append(c.Env, "SOFT_SERVE_LOG_FORMAT="+cfg.Log.Format)
-		c.Env = append(c.Env, "SOFT_SERVE_LOG_TIME_FORMAT="+cfg.Log.TimeFormat)
-	}
-
-	stdin, err := c.StdinPipe()
-	if err != nil {
-		logger.Error("failed to get stdin pipe", "err", err)
-		return err
-	}
-
-	stdout, err := c.StdoutPipe()
-	if err != nil {
-		logger.Error("failed to get stdout pipe", "err", err)
-		return err
-	}
-
-	stderr, err := c.StderrPipe()
-	if err != nil {
-		logger.Error("failed to get stderr pipe", "err", err)
-		return err
-	}
-
-	if err := c.Start(); err != nil {
-		logger.Error("failed to start command", "err", err)
-		return err
-	}
-
-	errg, ctx := errgroup.WithContext(ctx)
-
-	// stdin
-	errg.Go(func() error {
-		defer stdin.Close()
-
-		_, err := io.Copy(stdin, in)
-		return err
-	})
-
-	// stdout
-	errg.Go(func() error {
-		_, err := io.Copy(out, stdout)
-		return err
-	})
-
-	// stderr
-	errg.Go(func() error {
-		_, err := io.Copy(er, stderr)
-		return err
-	})
-
-	if err := errg.Wait(); err != nil {
-		logger.Error("while copying output", "err", err)
-	}
-
-	// Wait for the command to finish
-	return c.Wait()
-}
 
 // WritePktline encodes and writes a pktline to the given writer.
 func WritePktline(w io.Writer, v ...interface{}) {
@@ -179,19 +69,8 @@ func EnsureWithin(reposDir string, repo string) error {
 	return nil
 }
 
-func fileExists(path string) (bool, error) {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true, nil
-	}
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-	return true, err
-}
-
-func EnsureDefaultBranch(ctx context.Context, in io.Reader, out io.Writer, er io.Writer, repoPath string) error {
-	r, err := git.Open(repoPath)
+func EnsureDefaultBranch(ctx context.Context, scmd ServiceCommand) error {
+	r, err := git.Open(scmd.Dir)
 	if err != nil {
 		return err
 	}
@@ -205,8 +84,21 @@ func EnsureDefaultBranch(ctx context.Context, in io.Reader, out io.Writer, er io
 	// Rename the default branch to the first branch available
 	_, err = r.HEAD()
 	if err == git.ErrReferenceNotExist {
-		err = RunGit(ctx, in, out, er, repoPath, []string{}, "branch", "-M", brs[0])
-		if err != nil {
+		branch := brs[0]
+		// Prefer "main" or "master" as the default branch
+		for _, b := range brs {
+			if b == "main" || b == "master" {
+				branch = b
+				break
+			}
+		}
+
+		cmd := git.NewCommand("branch", "-M", branch).WithContext(ctx)
+		if err := cmd.RunInDirWithOptions(scmd.Dir, git.RunInDirOptions{
+			Stdin:  scmd.Stdin,
+			Stdout: scmd.Stdout,
+			Stderr: scmd.Stderr,
+		}); err != nil {
 			return err
 		}
 	}
---
+++ server/git/service.go
@@ -0,0 +1,136 @@
+package git
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+
+	"golang.org/x/sync/errgroup"
+)
+
+// Service is a Git daemon service.
+type Service string
+
+const (
+	// UploadPackService is the upload-pack service.
+	UploadPackService Service = "git-upload-pack"
+	// UploadArchiveService is the upload-archive service.
+	UploadArchiveService Service = "git-upload-archive"
+	// ReceivePackService is the receive-pack service.
+	ReceivePackService Service = "git-receive-pack"
+)
+
+// String returns the string representation of the service.
+func (s Service) String() string {
+	return string(s)
+}
+
+// Name returns the name of the service.
+func (s Service) Name() string {
+	return strings.TrimPrefix(s.String(), "git-")
+}
+
+// Handler is the service handler.
+func (s Service) Handler(ctx context.Context, cmd ServiceCommand) error {
+	switch s {
+	case UploadPackService, UploadArchiveService, ReceivePackService:
+		return gitServiceHandler(ctx, s, cmd)
+	default:
+		return fmt.Errorf("unsupported service: %s", s)
+	}
+}
+
+// ServiceHandler is a git service command handler.
+type ServiceHandler func(ctx context.Context, cmd ServiceCommand) error
+
+// gitServiceHandler is the default service handler using the git binary.
+func gitServiceHandler(ctx context.Context, svc Service, scmd ServiceCommand) error {
+	cmd := exec.CommandContext(ctx, "git", svc.Name()) // nolint: gosec
+	cmd.Dir = scmd.Dir
+	if len(scmd.Args) > 0 {
+		cmd.Args = append(cmd.Args, scmd.Args...)
+	}
+
+	cmd.Args = append(cmd.Args, ".")
+
+	cmd.Env = os.Environ()
+	if len(scmd.Env) > 0 {
+		cmd.Env = append(cmd.Env, scmd.Env...)
+	}
+
+	if scmd.CmdFunc != nil {
+		scmd.CmdFunc(cmd)
+	}
+
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return err
+	}
+
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	errg, ctx := errgroup.WithContext(ctx)
+
+	// stdin
+	errg.Go(func() error {
+		defer stdin.Close() // nolint: errcheck
+		_, err := io.Copy(stdin, scmd.Stdin)
+		return err
+	})
+
+	// stdout
+	errg.Go(func() error {
+		_, err := io.Copy(scmd.Stdout, stdout)
+		return err
+	})
+
+	// stderr
+	errg.Go(func() error {
+		_, err := io.Copy(scmd.Stderr, stderr)
+		return err
+	})
+
+	return errors.Join(errg.Wait(), cmd.Wait())
+}
+
+// ServiceCommand is used to run a git service command.
+type ServiceCommand struct {
+	Stdin   io.Reader
+	Stdout  io.Writer
+	Stderr  io.Writer
+	Dir     string
+	Env     []string
+	Args    []string
+	CmdFunc func(*exec.Cmd)
+}
+
+// UploadPack runs the git upload-pack protocol against the provided repo.
+func UploadPack(ctx context.Context, cmd ServiceCommand) error {
+	return gitServiceHandler(ctx, UploadPackService, cmd)
+}
+
+// UploadArchive runs the git upload-archive protocol against the provided repo.
+func UploadArchive(ctx context.Context, cmd ServiceCommand) error {
+	return gitServiceHandler(ctx, UploadArchiveService, cmd)
+}
+
+// ReceivePack runs the git receive-pack protocol against the provided repo.
+func ReceivePack(ctx context.Context, cmd ServiceCommand) error {
+	return gitServiceHandler(ctx, ReceivePackService, cmd)
+}
