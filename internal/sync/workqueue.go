charmbracelet/soft-serve

commit b07de747b54ae033d6f2cf2a061f852034a39911

fix(log): respect log config (#280)
* fix(log): respect log config

Parse log config

fix(git): ensure os envs are present

feat: set the default time format to dateTime

fix(log): change update mirror log message into debug

fix(config): rename log config struct

fix(config): always return cfg

* perf: update mirrors in a workpool (#285)

* perf: update mirrors in a workqueue

Implement a simple chunked workqueue to queue updating mirrors. We use
the number of cpus to calculate the number of workers to distribute the
work to.

* fix: use automaxprocs

Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

* fix: set maxprocs in main

* feat(wp): use a workpool impl

Use semaphores to implement a workpool of n workers
and use that to run the mirroring job

---------

Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
Co-authored-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

---------

Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
Co-authored-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

internal/sync/workqueue.go
@@ -1,0 +1,98 @@
+package sync
+
+import (
+	"context"
+	"sync"
+
+	"golang.org/x/sync/semaphore"
+)
+
+// WorkPool is a pool of work to be done.
+type WorkPool struct {
+	workers int
+	work    map[string]func()
+	mu      sync.RWMutex
+	sem     *semaphore.Weighted
+	ctx     context.Context
+	logger  func(string, ...interface{})
+}
+
+// WorkPoolOption is a function that configures a WorkPool.
+type WorkPoolOption func(*WorkPool)
+
+// WithWorkPoolLogger sets the logger to use.
+func WithWorkPoolLogger(logger func(string, ...interface{})) WorkPoolOption {
+	return func(wq *WorkPool) {
+		wq.logger = logger
+	}
+}
+
+// NewWorkPool creates a new work pool. The workers argument specifies the
+// number of concurrent workers to run the work.
+// The queue will chunk the work into batches of workers size.
+func NewWorkPool(ctx context.Context, workers int, opts ...WorkPoolOption) *WorkPool {
+	wq := &WorkPool{
+		workers: workers,
+		work:    make(map[string]func()),
+		ctx:     ctx,
+	}
+
+	for _, opt := range opts {
+		opt(wq)
+	}
+
+	if wq.workers <= 0 {
+		wq.workers = 1
+	}
+
+	wq.sem = semaphore.NewWeighted(int64(wq.workers))
+
+	return wq
+}
+
+// Run starts the workers and waits for them to finish.
+func (wq *WorkPool) Run() {
+	for id, fn := range wq.work {
+		if err := wq.sem.Acquire(wq.ctx, 1); err != nil {
+			wq.logf("workpool: %v", err)
+			return
+		}
+
+		go func(id string, fn func()) {
+			defer wq.sem.Release(1)
+			fn()
+			wq.mu.Lock()
+			delete(wq.work, id)
+			wq.mu.Unlock()
+		}(id, fn)
+	}
+
+	if err := wq.sem.Acquire(wq.ctx, int64(wq.workers)); err != nil {
+		wq.logf("workpool: %v", err)
+	}
+}
+
+// Add adds a new job to the pool.
+// If the job already exists, it is a no-op.
+func (wq *WorkPool) Add(id string, fn func()) {
+	wq.mu.Lock()
+	defer wq.mu.Unlock()
+	if _, ok := wq.work[id]; ok {
+		return
+	}
+	wq.work[id] = fn
+}
+
+// Status checks if a job is in the queue.
+func (wq *WorkPool) Status(id string) bool {
+	wq.mu.RLock()
+	defer wq.mu.RUnlock()
+	_, ok := wq.work[id]
+	return ok
+}
+
+func (wq *WorkPool) logf(format string, args ...interface{}) {
+	if wq.logger != nil {
+		wq.logger(format, args...)
+	}
+}

cmd/soft/root.go
@@ -8,6 +8,7 @@ import (
 	"github.com/charmbracelet/log"
 	. "github.com/charmbracelet/soft-serve/internal/log"
 	"github.com/spf13/cobra"
+	"go.uber.org/automaxprocs/maxprocs"
 )
 
 var (
@@ -52,6 +53,13 @@ func init() {
 
 func main() {
 	logger := NewDefaultLogger()
+
+	// Set the max number of processes to the number of CPUs
+	// This is useful when running soft serve in a container
+	if _, err := maxprocs.Set(maxprocs.Logger(logger.Debugf)); err != nil {
+		logger.Warn("couldn't set automaxprocs", "error", err)
+	}
+
 	ctx := log.WithContext(context.Background(), logger)
 	if err := rootCmd.ExecuteContext(ctx); err != nil {
 		os.Exit(1)

go.mod
@@ -32,6 +32,7 @@ require (
 	github.com/prometheus/client_golang v1.15.1
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/spf13/cobra v1.7.0
+	go.uber.org/automaxprocs v1.5.2
 	goji.io v2.0.2+incompatible
 	golang.org/x/crypto v0.9.0
 	golang.org/x/sync v0.2.0

go.sum
@@ -161,6 +161,7 @@ github.com/pjbgf/sha1cd v0.3.0/go.mod h1:nZ1rrWOcGJ5uZgEEVL1VUM9iRQiZvWdbZjkKyFz
 github.com/pkg/errors v0.9.1/go.mod h1:bwawxfHBFNV+L2hUp1rHADufV3IMtnDRdf1r5NINEl0=
 github.com/pmezard/go-difflib v1.0.0 h1:4DBwDE0NGyQoBHbLQYPwSUPoCMWR5BEzIk/f1lZbAQM=
 github.com/pmezard/go-difflib v1.0.0/go.mod h1:iKH77koFhYxTK1pcRnkKkqfTogsbg7gZNVY4sRDYZ/4=
+github.com/prashantv/gostub v1.1.0 h1:BTyx3RfQjRHnUWaGF9oQos79AlQ5k8WNktv7VGvVH4g=
 github.com/prometheus/client_golang v1.15.1 h1:8tXpTmJbyH5lydzFPoxSIJ0J46jdh3tylbvM1xCv0LI=
 github.com/prometheus/client_golang v1.15.1/go.mod h1:e9yaBhRPU2pPNsZwE+JdQl0KEt1N9XgF6zxWmaC0xOk=
 github.com/prometheus/client_model v0.3.0 h1:UBgGFHqYdG/TPFD1B1ogZywDqEkwp3fBMvqdiQ7Xew4=
@@ -207,6 +208,8 @@ github.com/yuin/goldmark v1.5.2 h1:ALmeCk/px5FSm1MAcFBAsVKZjDuMVj8Tm7FFIlMJnqU=
 github.com/yuin/goldmark v1.5.2/go.mod h1:6yULJ656Px+3vBD8DxQVa3kxgyrAnzto9xy5taEt/CY=
 github.com/yuin/goldmark-emoji v1.0.1 h1:ctuWEyzGBwiucEqxzwe0SOYDXPAucOrE9NQC18Wa1os=
 github.com/yuin/goldmark-emoji v1.0.1/go.mod h1:2w1E6FEWLcDQkoTE+7HU6QF1F6SLlNGjRIBbIZQFqkQ=
+go.uber.org/automaxprocs v1.5.2 h1:2LxUOGiR3O6tw8ui5sZa2LAaHnsviZdVOUZw4fvbnME=
+go.uber.org/automaxprocs v1.5.2/go.mod h1:eRbA25aqJrxAbsLO0xy5jVwPt7FQnRgjW+efnwa1WM0=
 goji.io v2.0.2+incompatible h1:uIssv/elbKRLznFUy3Xj4+2Mz/qKhek/9aZQDUMae7c=
 goji.io v2.0.2+incompatible/go.mod h1:sbqFwrtqZACxLBTQcdgVjFh54yGVCvwq8+w49MVMMIk=
 golang.org/x/arch v0.1.0/go.mod h1:5om86z9Hs0C8fWVUuoMHwpExlXzs5Tkyp9hOrfG7pp8=

internal/log/log.go
@@ -2,17 +2,29 @@ package log
 
 import (
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/charmbracelet/log"
+	"github.com/charmbracelet/soft-serve/server/config"
 )
 
 var contextKey = &struct{ string }{"logger"}
 
 // NewDefaultLogger returns a new logger with default settings.
 func NewDefaultLogger() *log.Logger {
+	dp := os.Getenv("SOFT_SERVE_DATA_PATH")
+	if dp == "" {
+		dp = "data"
+	}
+
+	cfg, err := config.ParseConfig(filepath.Join(dp, "config.yaml"))
+	if err != nil {
+		log.Errorf("failed to parse config: %v", err)
+	}
+
 	logger := log.NewWithOptions(os.Stderr, log.Options{
 		ReportTimestamp: true,
 		TimeFormat:      time.DateOnly,
@@ -22,11 +34,9 @@ func NewDefaultLogger() *log.Logger {
 		logger.SetLevel(log.DebugLevel)
 	}
 
-	if tsfmt := os.Getenv("SOFT_SERVE_LOG_TIME_FORMAT"); tsfmt != "" {
-		logger.SetTimeFormat(tsfmt)
-	}
+	logger.SetTimeFormat(cfg.Log.TimeFormat)
 
-	switch strings.ToLower(os.Getenv("SOFT_SERVE_LOG_FORMAT")) {
+	switch strings.ToLower(cfg.Log.Format) {
 	case "json":
 		logger.SetFormatter(log.JSONFormatter)
 	case "logfmt":

server/config/config.go
@@ -73,6 +73,17 @@ type StatsConfig struct {
 	ListenAddr string `env:"LISTEN_ADDR" yaml:"listen_addr"`
 }
 
+// LogConfig is the logger configuration.
+type LogConfig struct {
+	// Format is the format of the logs.
+	// Valid values are "json", "logfmt", and "text".
+	Format string `env:"FORMAT" yaml:"format"`
+
+	// Time format for the log `ts` field.
+	// Format must be described in Golang's time format.
+	TimeFormat string `env:"TIME_FORMAT" yaml:"time_format"`
+}
+
 // Config is the configuration for Soft Serve.
 type Config struct {
 	// Name is the name of the server.
@@ -90,13 +101,8 @@ type Config struct {
 	// Stats is the configuration for the stats server.
 	Stats StatsConfig `envPrefix:"STATS_" yaml:"stats"`
 
-	// LogFormat is the format of the logs.
-	// Valid values are "json", "logfmt", and "text".
-	LogFormat string `env:"LOG_FORMAT" yaml:"log_format"`
-
-	// Time format for the log `ts` field.
-	// Format must be described in Golang's time format.
-	LogTimeFormat string `env:"LOG_TIME_FORMAT" yaml:"log_time_format"`
+	// Log is the logger configuration.
+	Log LogConfig `envPrefix:"LOG_" yaml:"log"`
 
 	// InitialAdminKeys is a list of public keys that will be added to the list of admins.
 	InitialAdminKeys []string `env:"INITIAL_ADMIN_KEYS" envSeparator:"\n" yaml:"initial_admin_keys"`
@@ -111,10 +117,8 @@ type Config struct {
 func parseConfig(path string) (*Config, error) {
 	dataPath := filepath.Dir(path)
 	cfg := &Config{
-		Name:          "Soft Serve",
-		LogFormat:     "text",
-		LogTimeFormat: time.DateOnly,
-		DataPath:      dataPath,
+		Name:     "Soft Serve",
+		DataPath: dataPath,
 		SSH: SSHConfig{
 			ListenAddr:    ":23231",
 			PublicURL:     "ssh://localhost:23231",
@@ -135,6 +139,10 @@ func parseConfig(path string) (*Config, error) {
 		},
 		Stats: StatsConfig{
 			ListenAddr: "localhost:23233",
+		},
+		Log: LogConfig{
+			Format:     "text",
+			TimeFormat: time.DateTime,
 		},
 	}
 
@@ -182,11 +190,11 @@ func parseConfig(path string) (*Config, error) {
 func ParseConfig(path string) (*Config, error) {
 	cfg, err := parseConfig(path)
 	if err != nil {
-		return nil, err
+		return cfg, err
 	}
 
 	if err := cfg.validate(); err != nil {
-		return nil, err
+		return cfg, err
 	}
 
 	return cfg, nil

server/config/file.go
@@ -11,9 +11,13 @@ var configFileTmpl = template.Must(template.New("config").Parse(`# Soft Serve Se
 # This is the name that will be displayed in the UI.
 name: "{{ .Name }}"
 
-# Log format to use. Valid values are "json", "logfmt", and "text".
-log_format: "{{ .LogFormat }}"
-log_time_format: "{{ .LogTimeFormat }}"
+# Logging configuration.
+log:
+  # Log format to use. Valid values are "json", "logfmt", and "text".
+  format: "{{ .Log.Format }}"
+  # Time format for the log "timestamp" field.
+  # Should be described in Golang's time format.
+  time_format: "{{ .Log.TimeFormat }}"
 
 # The SSH server configuration.
 ssh:

server/git/git.go
@@ -83,12 +83,12 @@ func RunGit(ctx context.Context, in io.Reader, out io.Writer, er io.Writer, dir 
 	logger := log.FromContext(ctx).WithPrefix("rungit")
 	c := exec.CommandContext(ctx, "git", args...)
 	c.Dir = dir
-	c.Env = append(c.Env, envs...)
+	c.Env = append(os.Environ(), envs...)
 	c.Env = append(c.Env, "PATH="+os.Getenv("PATH"))
 	c.Env = append(c.Env, "SOFT_SERVE_DEBUG="+os.Getenv("SOFT_SERVE_DEBUG"))
 	if cfg != nil {
-		c.Env = append(c.Env, "SOFT_SERVE_LOG_FORMAT="+cfg.LogFormat)
-		c.Env = append(c.Env, "SOFT_SERVE_LOG_TIME_FORMAT="+cfg.LogTimeFormat)
+		c.Env = append(c.Env, "SOFT_SERVE_LOG_FORMAT="+cfg.Log.Format)
+		c.Env = append(c.Env, "SOFT_SERVE_LOG_TIME_FORMAT="+cfg.Log.TimeFormat)
 	}
 
 	stdin, err := c.StdinPipe()

commit a9e4824691f12a7c1e03161e9d60425a2d5ca637

fix(server): possible panic using maps
use a sync.Map to avoid possible panic on concurrent map iteration and
map write

Fixes: b07de747b54a ("fix(log): respect log config (#280)")

internal/sync/workqueue.go
@@ -10,8 +10,7 @@ import (
 // WorkPool is a pool of work to be done.
 type WorkPool struct {
 	workers int
-	work    map[string]func()
-	mu      sync.RWMutex
+	work    sync.Map
 	sem     *semaphore.Weighted
 	ctx     context.Context
 	logger  func(string, ...interface{})
@@ -33,7 +32,6 @@ func WithWorkPoolLogger(logger func(string, ...interface{})) WorkPoolOption {
 func NewWorkPool(ctx context.Context, workers int, opts ...WorkPoolOption) *WorkPool {
 	wq := &WorkPool{
 		workers: workers,
-		work:    make(map[string]func()),
 		ctx:     ctx,
 	}
 
@@ -52,20 +50,22 @@ func NewWorkPool(ctx context.Context, workers int, opts ...WorkPoolOption) *Work
 
 // Run starts the workers and waits for them to finish.
 func (wq *WorkPool) Run() {
-	for id, fn := range wq.work {
+	wq.work.Range(func(key, value any) bool {
+		id := key.(string)
+		fn := value.(func())
 		if err := wq.sem.Acquire(wq.ctx, 1); err != nil {
 			wq.logf("workpool: %v", err)
-			return
+			return false
 		}
 
 		go func(id string, fn func()) {
 			defer wq.sem.Release(1)
 			fn()
-			wq.mu.Lock()
-			delete(wq.work, id)
-			wq.mu.Unlock()
+			wq.work.Delete(id)
 		}(id, fn)
-	}
+
+		return true
+	})
 
 	if err := wq.sem.Acquire(wq.ctx, int64(wq.workers)); err != nil {
 		wq.logf("workpool: %v", err)
@@ -75,19 +75,15 @@ func (wq *WorkPool) Run() {
 // Add adds a new job to the pool.
 // If the job already exists, it is a no-op.
 func (wq *WorkPool) Add(id string, fn func()) {
-	wq.mu.Lock()
-	defer wq.mu.Unlock()
-	if _, ok := wq.work[id]; ok {
+	if _, ok := wq.work.Load(id); ok {
 		return
 	}
-	wq.work[id] = fn
+	wq.work.Store(id, fn)
 }
 
 // Status checks if a job is in the queue.
 func (wq *WorkPool) Status(id string) bool {
-	wq.mu.RLock()
-	defer wq.mu.RUnlock()
-	_, ok := wq.work[id]
+	_, ok := wq.work.Load(id)
 	return ok
 }
 