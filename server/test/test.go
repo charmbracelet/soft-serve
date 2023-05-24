b329b276e07d7d4fde3b86e402d603e78eeee192
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-05-24 16:36:57 +0000

    test: random port: prevent port reuse

--- server/test/test.go
+++ server/test/test.go
@@ -1,11 +1,29 @@
 package test
 
-import "net"
+import (
+	"net"
+	"sync"
+)
+
+var (
+	used = map[int]struct{}{}
+	lock sync.Mutex
+)
 
 // RandomPort returns a random port number.
 // This is mainly used for testing.
 func RandomPort() int {
 	addr, _ := net.Listen("tcp", ":0") //nolint:gosec
 	_ = addr.Close()
-	return addr.Addr().(*net.TCPAddr).Port
+	port := addr.Addr().(*net.TCPAddr).Port
+	lock.Lock()
+
+	if _, ok := used[port]; ok {
+		lock.Unlock()
+		return RandomPort()
+	}
+
+	used[port] = struct{}{}
+	lock.Unlock()
+	return port
 }
