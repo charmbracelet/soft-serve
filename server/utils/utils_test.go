0ac72c1f5a35e6378ab64ec4fee56dcb3a5dea1f
Ayman Bagabas <ayman.bagabas@gmail.com>
2023-05-25 13:06:27 -0400

    feat(test): add SanitizeRepo tests

--- server/utils/utils_test.go
+++ server/utils/utils_test.go
@@ -34,3 +34,22 @@ func TestValidateRepo(t *testing.T) {
 		}
 	})
 }
+
+func TestSanitizeRepo(t *testing.T) {
+	cases := []struct {
+		in, out string
+	}{
+		{"lower", "lower"},
+		{"Upper", "Upper"},
+		{"with/slash", "with/slash"},
+		{"with.dot", "with.dot"},
+		{"withgitsuffix.git", "withgitsuffix"},
+	}
+	for _, c := range cases {
+		t.Run(c.in, func(t *testing.T) {
+			if got := SanitizeRepo(c.in); got != c.out {
+				t.Errorf("expected %q, got %q", c.out, got)
+			}
+		})
+	}
+}
