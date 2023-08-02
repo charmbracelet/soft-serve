2bdbb4e255c71fc426734c13f0f1bfa6eca65cd2
Ayman Bagabas <ayman.bagabas@gmail.com>
2023-08-02 17:44:49 -0400

    feat,fix: add task manager
    Implement a task manager that can run different tasks given a unique ID.
    
    This is needed to accommodate expensive tasks like importing a large
    repository. The current behavior uses the connection's context (the SSH
    connection) to import the repository. However, if the server has defined
    an SSH `idle_timeout`, `max_timeout`, and/or the connection drops,
    Soft Serve cancels the git clone process and aborts importing the
    repository.
    
    Instead, we add the import task to the "task manager" and wait on the
    connection context. If a task already exists for the same repository,
    return `Error: import already in progress`.
    
    Fixes: https://github.com/charmbracelet/soft-serve/issues/348

--- server/backend/backend.go
+++ server/backend/backend.go
@@ -7,17 +7,19 @@ import (
 	"github.com/charmbracelet/soft-serve/server/config"
 	"github.com/charmbracelet/soft-serve/server/db"
 	"github.com/charmbracelet/soft-serve/server/store"
+	"github.com/charmbracelet/soft-serve/server/task"
 )
 
 // Backend is the Soft Serve backend that handles users, repositories, and
 // server settings management and operations.
 type Backend struct {
-	ctx    context.Context
-	cfg    *config.Config
-	db     *db.DB
-	store  store.Store
-	logger *log.Logger
-	cache  *cache
+	ctx     context.Context
+	cfg     *config.Config
+	db      *db.DB
+	store   store.Store
+	logger  *log.Logger
+	cache   *cache
+	manager *task.Manager
 }
 
 // New returns a new Soft Serve backend.
@@ -25,11 +27,12 @@ func New(ctx context.Context, cfg *config.Config, db *db.DB) *Backend {
 	dbstore := store.FromContext(ctx)
 	logger := log.FromContext(ctx).WithPrefix("backend")
 	b := &Backend{
-		ctx:    ctx,
-		cfg:    cfg,
-		db:     db,
-		store:  dbstore,
-		logger: logger,
+		ctx:     ctx,
+		cfg:     cfg,
+		db:      db,
+		store:   dbstore,
+		logger:  logger,
+		manager: task.NewManager(ctx),
 	}
 
 	// TODO: implement a proper caching interface
