882e701296476297f4e4dae081e487f3c130b52c
Ayman Bagabas <ayman.bagabas@gmail.com>
2023-08-03 12:30:50 -0400

    feat: add collaborators with access level
    Now you can add a collaborator with a specific access level
    
    Fixes: https://github.com/charmbracelet/soft-serve/issues/281

--- server/store/collab.go
+++ server/store/collab.go
@@ -3,6 +3,7 @@ package store
 import (
 	"context"
 
+	"github.com/charmbracelet/soft-serve/server/access"
 	"github.com/charmbracelet/soft-serve/server/db"
 	"github.com/charmbracelet/soft-serve/server/db/models"
 )
@@ -10,7 +11,7 @@ import (
 // CollaboratorStore is an interface for managing collaborators.
 type CollaboratorStore interface {
 	GetCollabByUsernameAndRepo(ctx context.Context, h db.Handler, username string, repo string) (models.Collab, error)
-	AddCollabByUsernameAndRepo(ctx context.Context, h db.Handler, username string, repo string) error
+	AddCollabByUsernameAndRepo(ctx context.Context, h db.Handler, username string, repo string, level access.AccessLevel) error
 	RemoveCollabByUsernameAndRepo(ctx context.Context, h db.Handler, username string, repo string) error
 	ListCollabsByRepo(ctx context.Context, h db.Handler, repo string) ([]models.Collab, error)
 	ListCollabsByRepoAsUsers(ctx context.Context, h db.Handler, repo string) ([]models.User, error)
