charmbracelet/soft-serve

commit b0eebaa36b8aef44923936e94855b10736e54965

fix: panic on ssh lfs transfer

server/ssh/cmd/git.go
@@ -88,7 +88,7 @@ var (
 		Subsystem: "git",
 		Name:      "lfs_transfer_seconds_total",
 		Help:      "The total time spent on git-lfs-transfer requests",
-	}, []string{"repo"})
+	}, []string{"repo", "operation"})
 
 	createRepoCounter = promauto.NewCounterVec(prometheus.CounterOpts{
 		Namespace: "soft_serve",

commit 09dedc4092f2a878cfba561fea871853d53cac16

fix: remove extra metrics label
Fixes: b0eebaa36b8a ("fix: panic on ssh lfs transfer")

server/ssh/cmd/git.go
@@ -74,7 +74,7 @@ var (
 		Subsystem: "git",
 		Name:      "upload_archive_seconds_total",
 		Help:      "The total time spent on git-upload-archive requests",
-	}, []string{"repo", "operation"})
+	}, []string{"repo"})
 
 	lfsAuthenticateSeconds = promauto.NewCounterVec(prometheus.CounterOpts{
 		Namespace: "soft_serve",