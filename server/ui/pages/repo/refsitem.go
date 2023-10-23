charmbracelet/soft-serve

commit fd024099500440e8564bd926130fc341a13f9802

feat(ui): add branch/tag commit date and hash
Fixes: https://github.com/charmbracelet/soft-serve/issues/382

server/ui/pages/repo/refsitem.go
@@ -3,6 +3,8 @@ package repo
 import (
 	"fmt"
 	"io"
+	"strings"
+	"time"
 
 	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/list"
@@ -10,11 +12,15 @@ import (
 	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/soft-serve/git"
 	"github.com/charmbracelet/soft-serve/server/ui/common"
+	"github.com/dustin/go-humanize"
+	"github.com/muesli/reflow/truncate"
 )
 
 // RefItem is a git reference item.
 type RefItem struct {
 	*git.Reference
+	*git.Tag
+	*git.Commit
 }
 
 // ID implements selector.IdentifiableItem.
@@ -51,7 +57,13 @@ func (cl RefItems) Swap(i, j int) { cl[i], cl[j] = cl[j], cl[i] }
 
 // Less implements sort.Interface.
 func (cl RefItems) Less(i, j int) bool {
-	return cl[i].Short() < cl[j].Short()
+	if cl[i].Commit != nil && cl[j].Commit != nil {
+		return cl[i].Commit.Author.When.After(cl[j].Commit.Author.When)
+	} else if cl[i].Commit != nil && cl[j].Commit == nil {
+		return true
+	} else {
+		return false
+	}
 }
 
 // RefItemDelegate is the delegate for the ref item.
@@ -83,46 +95,94 @@ func (d RefItemDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd {
 
 // Render implements list.ItemDelegate.
 func (d RefItemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
-	s := d.common.Styles.Ref
 	i, ok := listItem.(RefItem)
 	if !ok {
 		return
 	}
 
-	var st lipgloss.Style
-	var selector string
-
 	isTag := i.Reference.IsTag()
 	isActive := index == m.Index()
-
-	if isTag && isActive {
-		st = s.Active.ItemTag
-	} else if isTag {
-		st = s.Normal.ItemTag
-	} else if isActive {
-		st = s.Active.Item
-	} else {
-		st = s.Normal.Item
+	s := d.common.Styles.Ref
+	st := s.Normal
+	selector := "  "
+	if isActive {
+		st = s.Active
+		selector = s.ItemSelector.String()
 	}
 
-	if isActive {
-		selector = s.ItemSelector.String()
+	horizontalFrameSize := st.Base.GetHorizontalFrameSize()
+	var itemSt lipgloss.Style
+	if isTag && isActive {
+		itemSt = st.ItemTag
+	} else if isTag {
+		itemSt = st.ItemTag
+	} else if isActive {
+		itemSt = st.Item
 	} else {
-		selector = "  "
+		itemSt = st.Item
+	}
+
+	var hash string
+	c := i.Commit
+	if c != nil {
+		hash = c.ID.String()[:7]
 	}
 
 	ref := i.Short()
-	ref = s.ItemBranch.Render(ref)
-	refMaxWidth := m.Width() -
-		s.ItemSelector.GetMarginLeft() -
-		s.ItemSelector.GetWidth() -
-		s.Normal.Item.GetMarginLeft()
-	ref = common.TruncateString(ref, refMaxWidth)
-	ref = st.Render(ref)
+
+	var desc string
+	if isTag {
+		if c != nil {
+			date := c.Committer.When.Format("Jan 02")
+			if c.Committer.When.Year() != time.Now().Year() {
+				date += fmt.Sprintf(" %d", c.Committer.When.Year())
+			}
+			desc += " " + st.ItemDesc.Render(date)
+		}
+
+		t := i.Tag
+		if t != nil {
+			msgSt := st.ItemDesc.Copy().Faint(false)
+			msg := t.Message()
+			nl := strings.Index(msg, "\n")
+			if nl > 0 {
+				msg = msg[:nl]
+			}
+			msg = strings.TrimSpace(msg)
+			if msg != "" {
+				msg = common.TruncateString(msg, m.Width()-
+					horizontalFrameSize-
+					lipgloss.Width(selector)-
+					lipgloss.Width(ref)-
+					lipgloss.Width(hash)-
+					lipgloss.Width(desc)-3) // 3 is for the paddings and truncation symbol
+				desc = " " + msgSt.Render(msg) + desc
+			}
+		}
+	} else if c != nil {
+		on := "updated " + humanize.Time(c.Committer.When)
+		desc += " " + st.ItemDesc.Render(on)
+	}
+
+	ref = itemSt.Render(ref)
+	hash = st.ItemHash.Copy().
+		Align(lipgloss.Right).
+		PaddingLeft(1).
+		Width(m.Width() -
+			horizontalFrameSize -
+			lipgloss.Width(selector) -
+			lipgloss.Width(ref) -
+			lipgloss.Width(desc) - 1). // 1 is for the left padding
+		Render(hash)
 	fmt.Fprint(w,
 		d.common.Zone.Mark(
 			i.ID(),
-			fmt.Sprint(selector, ref),
+			st.Base.Render(
+				lipgloss.JoinVertical(lipgloss.Top,
+					truncate.String(selector+ref+desc+hash,
+						uint(m.Width()-horizontalFrameSize)),
+				),
+			),
 		),
 	)
 }

git/commit.go
@@ -4,27 +4,11 @@ import (
 	"github.com/gogs/git-module"
 )
 
-// ZeroHash is the zero hash.
-var ZeroHash Hash = git.EmptyID
-
-// Hash represents a git hash.
-type Hash string
-
-// String returns the string representation of a hash as a string.
-func (h Hash) String() string {
-	return string(h)
-}
-
-// SHA1 represents the hash as a SHA1.
-func (h Hash) SHA1() *git.SHA1 {
-	return git.MustIDFromString(h.String())
-}
+// ZeroID is the zero hash.
+const ZeroID = git.EmptyID
 
 // Commit is a wrapper around git.Commit with helper methods.
-type Commit struct {
-	*git.Commit
-	Hash Hash
-}
+type Commit = git.Commit
 
 // Commits is a list of commits.
 type Commits []*Commit

git/patch.go
@@ -115,14 +115,14 @@ func (f *DiffFileChange) Mode() git.EntryMode {
 
 // Files returns the diff files.
 func (f *DiffFile) Files() (from *DiffFileChange, to *DiffFileChange) {
-	if f.OldIndex != ZeroHash.String() {
+	if f.OldIndex != ZeroID {
 		from = &DiffFileChange{
 			hash: f.OldIndex,
 			name: f.OldName(),
 			mode: f.OldMode(),
 		}
 	}
-	if f.Index != ZeroHash.String() {
+	if f.Index != ZeroID {
 		to = &DiffFileChange{
 			hash: f.Index,
 			name: f.Name,
@@ -298,14 +298,14 @@ func writeFilePatchHeader(sb *strings.Builder, filePatch *DiffFile) {
 		lines = append(lines,
 			fmt.Sprintf("diff --git %s %s", srcPrefix+to.Name(), dstPrefix+to.Name()),
 			fmt.Sprintf("new file mode %o", to.Mode()),
-			fmt.Sprintf("index %s..%s", ZeroHash, to.Hash()),
+			fmt.Sprintf("index %s..%s", ZeroID, to.Hash()),
 		)
 		lines = appendPathLines(lines, "/dev/null", dstPrefix+to.Name(), isBinary)
 	case to == nil:
 		lines = append(lines,
 			fmt.Sprintf("diff --git %s %s", srcPrefix+from.Name(), dstPrefix+from.Name()),
 			fmt.Sprintf("deleted file mode %o", from.Mode()),
-			fmt.Sprintf("index %s..%s", from.Hash(), ZeroHash),
+			fmt.Sprintf("index %s..%s", from.Hash(), ZeroID),
 		)
 		lines = appendPathLines(lines, srcPrefix+from.Name(), "/dev/null", isBinary)
 	}

git/reference.go
@@ -18,22 +18,11 @@ const (
 // Reference is a wrapper around git.Reference with helper methods.
 type Reference struct {
 	*git.Reference
-	Hash Hash
 	path string // repo path
 }
 
 // ReferenceName is a Refspec wrapper.
 type ReferenceName string
-
-// NewReference creates a new reference.
-func NewReference(rp, refspec string) *Reference {
-	return &Reference{
-		Reference: &git.Reference{
-			Refspec: refspec,
-		},
-		path: rp,
-	}
-}
 
 // String returns the reference name i.e. refs/heads/master.
 func (r ReferenceName) String() string {
@@ -42,11 +31,7 @@ func (r ReferenceName) String() string {
 
 // Short returns the short name of the reference i.e. master.
 func (r ReferenceName) Short() string {
-	s := strings.Split(r.String(), "/")
-	if len(s) > 0 {
-		return s[len(s)-1]
-	}
-	return r.String()
+	return git.RefShortName(string(r))
 }
 
 // Name returns the reference name i.e. refs/heads/master.
@@ -62,15 +47,4 @@ func (r *Reference) IsBranch() bool {
 // IsTag returns true if the reference is a tag.
 func (r *Reference) IsTag() bool {
 	return strings.HasPrefix(r.Refspec, git.RefsTags)
-}
-
-// TargetHash returns the hash of the reference target.
-func (r *Reference) TargetHash() Hash {
-	if r.IsTag() {
-		id, err := git.ShowRefVerify(r.path, r.Refspec)
-		if err == nil {
-			return Hash(id)
-		}
-	}
-	return r.Hash
 }

git/repo.go
@@ -77,7 +77,6 @@ func (r *Repository) HEAD() (*Reference, error) {
 			ID:      hash,
 			Refspec: rn,
 		},
-		Hash: Hash(hash),
 		path: r.Path,
 	}, nil
 }
@@ -92,7 +91,6 @@ func (r *Repository) References() ([]*Reference, error) {
 	for _, ref := range refs {
 		rrefs = append(rrefs, &Reference{
 			Reference: ref,
-			Hash:      Hash(ref.ID),
 			path:      r.Path,
 		})
 	}
@@ -121,7 +119,7 @@ func (r *Repository) Tree(ref *Reference) (*Tree, error) {
 		}
 		ref = rref
 	}
-	return r.LsTree(ref.Hash.String())
+	return r.LsTree(ref.ID)
 }
 
 // TreePath returns the tree for the given path.
@@ -142,7 +140,7 @@ func (r *Repository) TreePath(ref *Reference, path string) (*Tree, error) {
 
 // Diff returns the diff for the given commit.
 func (r *Repository) Diff(commit *Commit) (*Diff, error) {
-	ddiff, err := r.Repository.Diff(commit.Hash.String(), DiffMaxFiles, DiffMaxFileLines, DiffMaxLineChars, git.DiffOptions{
+	ddiff, err := r.Repository.Diff(commit.ID.String(), DiffMaxFiles, DiffMaxFileLines, DiffMaxLineChars, git.DiffOptions{
 		CommandOptions: git.CommandOptions{
 			Envs: []string{"GIT_CONFIG_GLOBAL=/dev/null"},
 		},
@@ -192,10 +190,7 @@ func (r *Repository) CommitsByPage(ref *Reference, page, size int) (Commits, err
 	}
 	commits := make(Commits, len(cs))
 	for i, c := range cs {
-		commits[i] = &Commit{
-			Commit: c,
-			Hash:   Hash(c.ID.String()),
-		}
+		commits[i] = c
 	}
 	return commits, nil
 }

git/tag.go
@@ -1,0 +1,5 @@
+package git
+
+import "github.com/gogs/git-module"
+
+type Tag = git.Tag

server/ssh/cmd/blob.go
@@ -60,7 +60,7 @@ func blobCommand() *cobra.Command {
 				if err != nil {
 					return err
 				}
-				ref = head.Hash.String()
+				ref = head.ID
 			}
 
 			tree, err := r.LsTree(ref)

server/ssh/cmd/commit.go
@@ -40,14 +40,9 @@ func commitCommand() *cobra.Command {
 				return err
 			}
 
-			rawCommit, err := r.CommitByRevision(commitSHA)
+			commit, err := r.CommitByRevision(commitSHA)
 			if err != nil {
 				return err
-			}
-
-			commit := &git.Commit{
-				Commit: rawCommit,
-				Hash:   git.Hash(commitSHA),
 			}
 
 			patch, err := r.Patch(commit)

commit 58006a384453a1ec3b0c2edbc00ce9da98edc98f

fix(ui): truncate refitem msg
Fixes: fd0240995004 (feat(ui): add branch/tag commit date and hash)

server/ui/pages/repo/refsitem.go
@@ -122,10 +122,10 @@ func (d RefItemDelegate) Render(w io.Writer, m list.Model, index int, listItem l
 		itemSt = st.Item
 	}
 
-	var hash string
+	var sha string
 	c := i.Commit
 	if c != nil {
-		hash = c.ID.String()[:7]
+		sha = c.ID.String()[:7]
 	}
 
 	ref := i.Short()
@@ -150,35 +150,53 @@ func (d RefItemDelegate) Render(w io.Writer, m list.Model, index int, listItem l
 			}
 			msg = strings.TrimSpace(msg)
 			if msg != "" {
-				msg = common.TruncateString(msg, m.Width()-
-					horizontalFrameSize-
-					lipgloss.Width(selector)-
-					lipgloss.Width(ref)-
-					lipgloss.Width(hash)-
-					lipgloss.Width(desc)-3) // 3 is for the paddings and truncation symbol
-				desc = " " + msgSt.Render(msg) + desc
+				msgMargin := m.Width() -
+					horizontalFrameSize -
+					lipgloss.Width(selector) -
+					lipgloss.Width(ref) -
+					lipgloss.Width(desc) -
+					lipgloss.Width(sha) -
+					3 // 3 is for the paddings and truncation symbol
+				if msgMargin >= 0 {
+					msg = common.TruncateString(msg, msgMargin)
+					desc = " " + msgSt.Render(msg) + desc
+				}
 			}
 		}
 	} else if c != nil {
-		on := "updated " + humanize.Time(c.Committer.When)
-		desc += " " + st.ItemDesc.Render(on)
-	}
-
-	ref = itemSt.Render(ref)
-	hash = st.ItemHash.Copy().
-		Align(lipgloss.Right).
-		PaddingLeft(1).
-		Width(m.Width() -
+		onMargin := m.Width() -
 			horizontalFrameSize -
 			lipgloss.Width(selector) -
 			lipgloss.Width(ref) -
-			lipgloss.Width(desc) - 1). // 1 is for the left padding
-		Render(hash)
+			lipgloss.Width(desc) -
+			lipgloss.Width(sha) -
+			2 // 2 is for the padding and truncation symbol
+		if onMargin >= 0 {
+			on := common.TruncateString("updated "+humanize.Time(c.Committer.When), onMargin)
+			desc += " " + st.ItemDesc.Render(on)
+		}
+	}
+
+	var hash string
+	ref = itemSt.Render(ref)
+	hashMargin := m.Width() -
+		horizontalFrameSize -
+		lipgloss.Width(selector) -
+		lipgloss.Width(ref) -
+		lipgloss.Width(desc) -
+		lipgloss.Width(sha) -
+		1 // 1 is for the left padding
+	if hashMargin >= 0 {
+		hash = strings.Repeat(" ", hashMargin) + st.ItemHash.Copy().
+			Align(lipgloss.Right).
+			PaddingLeft(1).
+			Render(sha)
+	}
 	fmt.Fprint(w,
 		d.common.Zone.Mark(
 			i.ID(),
 			st.Base.Render(
-				lipgloss.JoinHorizontal(lipgloss.Top,
+				lipgloss.JoinHorizontal(lipgloss.Left,
 					truncate.String(selector+ref+desc+hash,
 						uint(m.Width()-horizontalFrameSize)),
 				),