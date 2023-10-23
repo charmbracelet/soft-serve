charmbracelet/soft-serve

commit 43b4331f88ffd395098aa4332deeafa4cd9aa3fb

fix(ui): preserve header line when no description is available

server/ui/pages/repo/repo.go
@@ -84,13 +84,22 @@ func New(c common.Common, comps ...common.TabComponent) *Repo {
 	return r
 }
 
+func (r *Repo) getMargins() (w, h int) {
+	hh := 1
+	if r.selectedRepo != nil && r.selectedRepo.Description() != "" {
+		hh++
+	}
+	hm := r.common.Styles.Repo.Body.GetVerticalFrameSize() +
+		hh +
+		r.common.Styles.Repo.Header.GetVerticalFrameSize() +
+		r.common.Styles.StatusBar.GetHeight()
+	return 0, hm
+}
+
 // SetSize implements common.Component.
 func (r *Repo) SetSize(width, height int) {
 	r.common.SetSize(width, height)
-	hm := r.common.Styles.Repo.Body.GetVerticalFrameSize() +
-		r.common.Styles.Repo.Header.GetHeight() +
-		r.common.Styles.Repo.Header.GetVerticalFrameSize() +
-		r.common.Styles.StatusBar.GetHeight()
+	_, hm := r.getMargins()
 	r.tabs.SetSize(width, height-hm)
 	r.statusbar.SetSize(width, height-hm)
 	for _, p := range r.panes {
@@ -272,11 +281,8 @@ func (r *Repo) View() string {
 		Width(r.common.Width).
 		Height(r.common.Height)
 	repoBodyStyle := r.common.Styles.Repo.Body.Copy()
-	hm := repoBodyStyle.GetVerticalFrameSize() +
-		r.common.Styles.Repo.Header.GetHeight() +
-		r.common.Styles.Repo.Header.GetVerticalFrameSize() +
-		r.common.Styles.StatusBar.GetHeight() +
-		r.common.Styles.Tabs.GetHeight() +
+	_, hm := r.getMargins()
+	hm += r.common.Styles.Tabs.GetHeight() +
 		r.common.Styles.Tabs.GetVerticalFrameSize()
 	mainStyle := repoBodyStyle.
 		Height(r.common.Height - hm)
@@ -307,17 +313,17 @@ func (r *Repo) headerView() string {
 		return ""
 	}
 	truncate := lipgloss.NewStyle().MaxWidth(r.common.Width)
-	name := r.selectedRepo.ProjectName()
-	if name == "" {
-		name = r.selectedRepo.Name()
+	header := r.selectedRepo.ProjectName()
+	if header == "" {
+		header = r.selectedRepo.Name()
 	}
-	name = r.common.Styles.Repo.HeaderName.Render(name)
+	header = r.common.Styles.Repo.HeaderName.Render(header)
 	desc := strings.TrimSpace(r.selectedRepo.Description())
-	if desc == "" {
-		desc = name
-		name = ""
-	} else {
-		desc = r.common.Styles.Repo.HeaderDesc.Render(desc)
+	if desc != "" {
+		header = lipgloss.JoinVertical(lipgloss.Top,
+			header,
+			r.common.Styles.Repo.HeaderDesc.Render(desc),
+		)
 	}
 	urlStyle := r.common.Styles.URLStyle.Copy().
 		Width(r.common.Width - lipgloss.Width(desc) - 1).
@@ -331,15 +337,12 @@ func (r *Repo) headerView() string {
 		fmt.Sprintf("%s-url", r.selectedRepo.Name()),
 		urlStyle.Render(url),
 	)
+
+	header = lipgloss.JoinHorizontal(lipgloss.Left, header, url)
+
 	style := r.common.Styles.Repo.Header.Copy().Width(r.common.Width)
 	return style.Render(
-		lipgloss.JoinVertical(lipgloss.Top,
-			truncate.Render(name),
-			truncate.Render(lipgloss.JoinHorizontal(lipgloss.Left,
-				desc,
-				url,
-			)),
-		),
+		truncate.Render(header),
 	)
 }
 

server/ui/styles/styles.go
@@ -233,7 +233,7 @@ func DefaultStyles() *Styles {
 		Margin(1, 0)
 
 	s.Repo.Header = lipgloss.NewStyle().
-		Height(2).
+		MaxHeight(2).
 		Border(lipgloss.NormalBorder(), false, false, true, false).
 		BorderForeground(lipgloss.Color("236"))
 

commit 6bf930960fa74bb3ec6afe840c3a2d2dba876f7d

fix(ui): header height calculation
Fixes: 43b4331f88ff (fix(ui): preserve header line when no description is available)

server/ui/pages/repo/repo.go
@@ -84,11 +84,8 @@ func New(c common.Common, comps ...common.TabComponent) *Repo {
 	return r
 }
 
-func (r *Repo) getMargins() (w, h int) {
-	hh := 1
-	if r.selectedRepo != nil && r.selectedRepo.Description() != "" {
-		hh++
-	}
+func (r *Repo) getMargins() (int, int) {
+	hh := lipgloss.Height(r.headerView())
 	hm := r.common.Styles.Repo.Body.GetVerticalFrameSize() +
 		hh +
 		r.common.Styles.Repo.Header.GetVerticalFrameSize() +
@@ -277,14 +274,13 @@ func (r *Repo) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 // View implements tea.Model.
 func (r *Repo) View() string {
-	s := r.common.Styles.Repo.Base.Copy().
-		Width(r.common.Width).
-		Height(r.common.Height)
-	repoBodyStyle := r.common.Styles.Repo.Body.Copy()
-	_, hm := r.getMargins()
+	wm, hm := r.getMargins()
 	hm += r.common.Styles.Tabs.GetHeight() +
 		r.common.Styles.Tabs.GetVerticalFrameSize()
-	mainStyle := repoBodyStyle.
+	s := r.common.Styles.Repo.Base.Copy().
+		Width(r.common.Width - wm).
+		Height(r.common.Height - hm)
+	mainStyle := r.common.Styles.Repo.Body.Copy().
 		Height(r.common.Height - hm)
 	var main string
 	var statusbar string