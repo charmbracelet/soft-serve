7121f1622d9ef7c6b007a69814950bdaeca6ede2
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-10-31 18:36:20 +0000

    fix: use env to generate config.Environ()
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -10,24 +10,23 @@ import (
 func TestParseMultipleKeys(t *testing.T) {
 	is := is.New(t)
 	td := t.TempDir()
-	is.NoErr(os.Setenv("SOFT_SERVE_INITIAL_ADMIN_KEYS", "testdata/k1.pub\nssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFxIobhwtfdwN7m1TFt9wx3PsfvcAkISGPxmbmbauST8 a@b"))
-	is.NoErr(os.Setenv("SOFT_SERVE_DATA_PATH", td))
-	t.Cleanup(func() {
-		is.NoErr(os.Unsetenv("SOFT_SERVE_INITIAL_ADMIN_KEYS"))
-		is.NoErr(os.Unsetenv("SOFT_SERVE_DATA_PATH"))
-	})
+	t.Setenv("SOFT_SERVE_INITIAL_ADMIN_KEYS", "testdata/k1.pub\nssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFxIobhwtfdwN7m1TFt9wx3PsfvcAkISGPxmbmbauST8 a@b")
+	t.Setenv("SOFT_SERVE_DATA_PATH", td)
 	cfg := DefaultConfig()
 	is.NoErr(cfg.ParseEnv())
 	is.Equal(cfg.InitialAdminKeys, []string{
 		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINMwLvyV3ouVrTysUYGoJdl5Vgn5BACKov+n9PlzfPwH",
 		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFxIobhwtfdwN7m1TFt9wx3PsfvcAkISGPxmbmbauST8",
 	})
+	is.Equal(cfg.Environ(), []string{
+		"SOFT_SERVE_INITIAL_ADMIN_KEYS=testdata/k1.pub\nssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFxIobhwtfdwN7m1TFt9wx3PsfvcAkISGPxmbmbauST8 a@b",
+		"SOFT_SERVE_DATA_PATH=" + td,
+	})
 }
 
 func TestMergeInitAdminKeys(t *testing.T) {
 	is := is.New(t)
-	is.NoErr(os.Setenv("SOFT_SERVE_INITIAL_ADMIN_KEYS", "testdata/k1.pub"))
-	t.Cleanup(func() { is.NoErr(os.Unsetenv("SOFT_SERVE_INITIAL_ADMIN_KEYS")) })
+	t.Setenv("SOFT_SERVE_INITIAL_ADMIN_KEYS", "testdata/k1.pub")
 	cfg := &Config{
 		DataPath:         t.TempDir(),
 		InitialAdminKeys: []string{"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFxIobhwtfdwN7m1TFt9wx3PsfvcAkISGPxmbmbauST8 a@b"},
