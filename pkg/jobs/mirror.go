charmbracelet/soft-serve

commit 920e4a75d11de2f06010d23aab444ab2f02d5d70

fix: use lipgloss renderers
Fixes: https://github.com/charmbracelet/soft-serve/pull/468

pkg/jobs/mirror.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"path/filepath"
 	"runtime"
+	"strings"
 
 	"github.com/charmbracelet/log"
 	"github.com/charmbracelet/soft-serve/git"
@@ -62,16 +63,26 @@ func (m mirrorPull) Func(ctx context.Context) func() {
 				name := repo.Name()
 				wq.Add(name, func() {
 					repo := repo
-					cmd := git.NewCommand("remote", "update", "--prune").WithContext(ctx)
-					cmd.AddEnvs(
-						fmt.Sprintf(`GIT_SSH_COMMAND=ssh -o UserKnownHostsFile="%s" -o StrictHostKeyChecking=no -i "%s"`,
-							filepath.Join(cfg.DataPath, "ssh", "known_hosts"),
-							cfg.SSH.ClientKeyPath,
-						),
-					)
 
-					if _, err := cmd.RunInDir(r.Path); err != nil {
-						logger.Error("error running git remote update", "repo", name, "err", err)
+					cmds := []string{
+						"fetch --prune",               // fetch prune before updating remote
+						"gc --aggressive --prune=now", // aggressive garbage collection
+						"remote update --prune",       // update remote and prune remote refs
+					}
+
+					for _, c := range cmds {
+						args := strings.Split(c, " ")
+						cmd := git.NewCommand(args...).WithContext(ctx)
+						cmd.AddEnvs(
+							fmt.Sprintf(`GIT_SSH_COMMAND=ssh -o UserKnownHostsFile="%s" -o StrictHostKeyChecking=no -i "%s"`,
+								filepath.Join(cfg.DataPath, "ssh", "known_hosts"),
+								cfg.SSH.ClientKeyPath,
+							),
+						)
+
+						if _, err := cmd.RunInDir(r.Path); err != nil {
+							logger.Error("error running git remote update", "repo", name, "err", err)
+						}
 					}
 
 					if cfg.LFS.Enabled {

pkg/ssh/cmd/blob.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/soft-serve/git"
 	"github.com/charmbracelet/soft-serve/pkg/backend"
 	"github.com/charmbracelet/soft-serve/pkg/ui/common"
@@ -11,12 +12,12 @@ import (
 )
 
 // blobCommand returns a command that prints the contents of a file.
-func blobCommand() *cobra.Command {
+func blobCommand(renderer *lipgloss.Renderer) *cobra.Command {
 	var linenumber bool
 	var color bool
 	var raw bool
 
-	styles := styles.DefaultStyles()
+	styles := styles.DefaultStyles(renderer)
 	cmd := &cobra.Command{
 		Use:               "blob REPOSITORY [REFERENCE] [PATH]",
 		Aliases:           []string{"cat", "show"},

pkg/ssh/cmd/commit.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	gansi "github.com/charmbracelet/glamour/ansi"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/soft-serve/git"
 	"github.com/charmbracelet/soft-serve/pkg/backend"
 	"github.com/charmbracelet/soft-serve/pkg/ui/common"
@@ -14,7 +15,7 @@ import (
 )
 
 // commitCommand returns a command that prints the contents of a commit.
-func commitCommand() *cobra.Command {
+func commitCommand(renderer *lipgloss.Renderer) *cobra.Command {
 	var color bool
 	var patchOnly bool
 
@@ -54,7 +55,7 @@ func commitCommand() *cobra.Command {
 				return err
 			}
 
-			commonStyle := styles.DefaultStyles()
+			commonStyle := styles.DefaultStyles(renderer)
 			style := commonStyle.Log
 
 			s := strings.Builder{}

pkg/ssh/cmd/repo.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/soft-serve/pkg/backend"
 	"github.com/charmbracelet/soft-serve/pkg/proto"
 	"github.com/spf13/cobra"
 )
 
 // RepoCommand returns a command for managing repositories.
-func RepoCommand() *cobra.Command {
+func RepoCommand(renderer *lipgloss.Renderer) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     "repo",
 		Aliases: []string{"repos", "repository", "repositories"},
@@ -18,10 +19,10 @@ func RepoCommand() *cobra.Command {
 	}
 
 	cmd.AddCommand(
-		blobCommand(),
+		blobCommand(renderer),
 		branchCommand(),
 		collabCommand(),
-		commitCommand(),
+		commitCommand(renderer),
 		createCommand(),
 		deleteCommand(),
 		descriptionCommand(),

pkg/ssh/middleware.go
@@ -2,6 +2,7 @@ package ssh
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/charmbracelet/log"
@@ -14,6 +15,8 @@ import (
 	"github.com/charmbracelet/soft-serve/pkg/store"
 	"github.com/charmbracelet/ssh"
 	"github.com/charmbracelet/wish"
+	bm "github.com/charmbracelet/wish/bubbletea"
+	"github.com/muesli/termenv"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/spf13/cobra"
@@ -87,6 +90,12 @@ func CommandMiddleware(sh ssh.Handler) ssh.Handler {
 		ctx := s.Context()
 		cfg := config.FromContext(ctx)
 
+		renderer := bm.MakeRenderer(s)
+		if testrun, ok := os.LookupEnv("SOFT_SERVE_NO_COLOR"); ok && testrun == "1" {
+			// Disable colors when running tests.
+			renderer.SetColorProfile(termenv.Ascii)
+		}
+
 		args := s.Command()
 		cliCommandCounter.WithLabelValues(cmd.CommandName(args)).Inc()
 		rootCmd := &cobra.Command{
@@ -101,7 +110,7 @@ func CommandMiddleware(sh ssh.Handler) ssh.Handler {
 			cmd.GitUploadPackCommand(),
 			cmd.GitUploadArchiveCommand(),
 			cmd.GitReceivePackCommand(),
-			cmd.RepoCommand(),
+			cmd.RepoCommand(renderer),
 			cmd.SettingsCommand(),
 			cmd.UserCommand(),
 			cmd.InfoCommand(),

pkg/ssh/session.go
@@ -1,6 +1,7 @@
 package ssh
 
 import (
+	"os"
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -12,6 +13,7 @@ import (
 	"github.com/charmbracelet/ssh"
 	"github.com/charmbracelet/wish"
 	bm "github.com/charmbracelet/wish/bubbletea"
+	"github.com/muesli/termenv"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
@@ -52,8 +54,13 @@ func SessionHandler(s ssh.Session) *tea.Program {
 		}
 	}
 
-	output := bm.MakeRenderer(s)
-	c := common.NewCommon(ctx, output, pty.Window.Width, pty.Window.Height)
+	renderer := bm.MakeRenderer(s)
+	if testrun, ok := os.LookupEnv("SOFT_SERVE_NO_COLOR"); ok && testrun == "1" {
+		// Disable colors when running tests.
+		renderer.SetColorProfile(termenv.Ascii)
+	}
+
+	c := common.NewCommon(ctx, renderer, pty.Window.Width, pty.Window.Height)
 	c.SetValue(common.ConfigKey, cfg)
 	m := NewUI(c, initialRepo)
 	opts := bm.MakeOptions(s)

pkg/ui/common/common.go
@@ -34,6 +34,7 @@ type Common struct {
 	Styles        *styles.Styles
 	KeyMap        *keymap.KeyMap
 	Zone          *zone.Manager
+	Renderer      *lipgloss.Renderer
 	Output        *termenv.Output
 	Logger        *log.Logger
 	HideCloneCmd  bool
@@ -45,14 +46,15 @@ func NewCommon(ctx context.Context, out *lipgloss.Renderer, width, height int) C
 		ctx = context.TODO()
 	}
 	return Common{
-		ctx:    ctx,
-		Width:  width,
-		Height: height,
-		Output: out.Output(),
-		Styles: styles.DefaultStyles(),
-		KeyMap: keymap.DefaultKeyMap(),
-		Zone:   zone.New(),
-		Logger: log.FromContext(ctx).WithPrefix("ui"),
+		ctx:      ctx,
+		Width:    width,
+		Height:   height,
+		Renderer: out,
+		Output:   out.Output(),
+		Styles:   styles.DefaultStyles(out),
+		KeyMap:   keymap.DefaultKeyMap(),
+		Zone:     zone.New(),
+		Logger:   log.FromContext(ctx).WithPrefix("ui"),
 	}
 }
 

pkg/ui/components/code/code.go
@@ -121,7 +121,7 @@ func (r *Code) Init() tea.Cmd {
 	// https://github.com/muesli/reflow/issues/43
 	//
 	// TODO: solve this upstream in Glamour/Reflow.
-	content = lipgloss.NewStyle().Width(w).Render(content)
+	content = r.common.Renderer.NewStyle().Width(w).Render(content)
 
 	r.Viewport.Model.SetContent(content)
 

commit b15f8740f185d8c4e63c9141ef06546cd8da1794

fix(mirror): don't need git gc
Fixes: 920e4a75d11d (fix: use lipgloss renderers)

pkg/jobs/mirror.go
@@ -65,9 +65,8 @@ func (m mirrorPull) Func(ctx context.Context) func() {
 					repo := repo
 
 					cmds := []string{
-						"fetch --prune",               // fetch prune before updating remote
-						"gc --aggressive --prune=now", // aggressive garbage collection
-						"remote update --prune",       // update remote and prune remote refs
+						"fetch --prune",         // fetch prune before updating remote
+						"remote update --prune", // update remote and prune remote refs
 					}
 
 					for _, c := range cmds {