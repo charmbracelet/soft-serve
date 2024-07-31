charmbracelet/soft-serve

commit 4daebdd422a6ba8c04162d023f8be355a8fe3184

sec: do not append session envs to git run (#544)
Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

pkg/ssh/cmd/git.go
@@ -202,9 +202,6 @@ func gitRunE(cmd *cobra.Command, args []string) error {
 		)
 	}
 
-	// Add ssh session & config environ
-	s := sshutils.SessionFromContext(ctx)
-	envs = append(envs, s.Environ()...)
 	envs = append(envs, cfg.Environ()...)
 
 	repoPath := filepath.Join(reposDir, repoDir)

commit f23ea48c913d6f907f499e0315c4658632d20d29

fix(ssh): pass GIT_PROTOCOL from ssh session down to binary
Fixes: 4daebdd422a6 (sec: do not append session envs to git run (#544))

pkg/ssh/cmd/git.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"errors"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/charmbracelet/log"
@@ -203,6 +204,16 @@ func gitRunE(cmd *cobra.Command, args []string) error {
 	}
 
 	envs = append(envs, cfg.Environ()...)
+
+	// Add GIT_PROTOCOL from session.
+	if sess := sshutils.SessionFromContext(ctx); sess != nil {
+		for _, env := range sess.Environ() {
+			if strings.HasPrefix(env, "GIT_PROTOCOL=") {
+				envs = append(envs, env)
+				break
+			}
+		}
+	}
 
 	repoPath := filepath.Join(reposDir, repoDir)
 	service := git.Service(cmd.Name())