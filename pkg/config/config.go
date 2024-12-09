charmbracelet/soft-serve

commit c354d5f21134b5cfa65c5fc6b6b9ff0dcf691bd2

feat: optionally pull config from a custom file (envvar), default to data path. (#557)
* feat: optionally pull config from a custom file (envvar), default to data path.

* docs: add docs on SOFT_SERVE_CONFIG_LOCATION

* feat: add tests for SOFT_SERVE_CONFIG_LOCATION

pkg/config/config.go
@@ -291,6 +291,12 @@ func DefaultDataPath() string {
 
 // ConfigPath returns the path to the config file.
 func (c *Config) ConfigPath() string { // nolint:revive
+	// If we have a custom config location set, then use that.
+	if path := os.Getenv("SOFT_SERVE_CONFIG_LOCATION"); exist(path) {
+		return path
+	}
+
+	// Otherwise, look in the data path.
 	return filepath.Join(c.DataPath, "config.yaml")
 }
 
@@ -301,7 +307,7 @@ func exist(path string) bool {
 
 // Exist returns true if the config file exists.
 func (c *Config) Exist() bool {
-	return exist(filepath.Join(c.DataPath, "config.yaml"))
+	return exist(c.ConfigPath())
 }
 
 // DefaultConfig returns the default Config. All the path values are relative

pkg/config/config_test.go
@@ -56,3 +56,26 @@ func TestValidateInitAdminKeys(t *testing.T) {
 		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINMwLvyV3ouVrTysUYGoJdl5Vgn5BACKov+n9PlzfPwH",
 	})
 }
+
+func TestCustomConfigLocation(t *testing.T) {
+	is := is.New(t)
+	td := t.TempDir()
+	t.Cleanup(func() {
+		is.NoErr(os.Unsetenv("SOFT_SERVE_CONFIG_LOCATION"))
+	})
+
+	// Test that we get data from the custom file location, and not from the data dir.
+	is.NoErr(os.Setenv("SOFT_SERVE_CONFIG_LOCATION", "testdata/config.yaml"))
+	is.NoErr(os.Setenv("SOFT_SERVE_DATA_PATH", td))
+	cfg := DefaultConfig()
+	is.NoErr(cfg.Parse())
+	is.Equal(cfg.Name, "Test server name")
+	// If we unset the custom location, then use the default location.
+	is.NoErr(os.Unsetenv("SOFT_SERVE_CONFIG_LOCATION"))
+	cfg = DefaultConfig()
+	is.Equal(cfg.Name, "Soft Serve")
+	// Test that if the custom config location doesn't exist, default to datapath config.
+	is.NoErr(os.Setenv("SOFT_SERVE_CONFIG_LOCATION", "testdata/config_nonexistent.yaml"))
+	cfg = DefaultConfig()
+	is.Equal(cfg.Name, "Soft Serve")
+}

pkg/config/testdata/config.yaml
@@ -1,0 +1,3 @@
+# Soft Serve Server configurations
+
+name: "Test server name"

commit c78da07b43142f7228e1b345d3cd4618a6d626a1

fix(config): add SOFT_SERVE_CONFIG_LOCATION to Environ
Fixes: c354d5f21134 (feat: optionally pull config from a custom file (envvar), default to data path. (#557))

pkg/config/config.go
@@ -174,6 +174,7 @@ func (c *Config) Environ() []string {
 
 	// TODO: do this dynamically
 	envs = append(envs, []string{
+		fmt.Sprintf("SOFT_SERVE_CONFIG_LOCATION=%s", c.ConfigPath()),
 		fmt.Sprintf("SOFT_SERVE_DATA_PATH=%s", c.DataPath),
 		fmt.Sprintf("SOFT_SERVE_NAME=%s", c.Name),
 		fmt.Sprintf("SOFT_SERVE_INITIAL_ADMIN_KEYS=%s", strings.Join(c.InitialAdminKeys, "\n")),