d5bac93efb35b226b1ea393c3ebb29dfb659f736
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-01-07 10:41:21 +0300

    fix: prevent path traversal attacks
    This commit fixes a path traversal vulnerability in the repository
    management code. The `SanitizeRepo` function now correctly returns a
    sanitized version of the given repository name. It uses an absolute
    path along with `path.Clean` to ensure that the path is cleaned
    before being used.

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -9,12 +9,15 @@ import (
 
 // SanitizeRepo returns a sanitized version of the given repository name.
 func SanitizeRepo(repo string) string {
+	// We need to use an absolute path for the path to be cleaned correctly.
 	repo = strings.TrimPrefix(repo, "/")
+	repo = "/" + repo
+
 	// We're using path instead of filepath here because this is not OS dependent
 	// looking at you Windows
 	repo = path.Clean(repo)
 	repo = strings.TrimSuffix(repo, ".git")
-	return repo
+	return repo[1:]
 }
 
 // ValidateUsername returns an error if any of the given usernames are invalid.
