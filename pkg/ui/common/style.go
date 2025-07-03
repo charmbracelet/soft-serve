a9140d3a46342877319917223dd8e54a9d500c6e
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-07-03 13:18:29 -0400

    chore(ui): common: remove unused style config

--- pkg/ui/common/style.go
+++ pkg/ui/common/style.go
@@ -17,13 +17,6 @@ func strptr(s string) *string {
 func StyleConfig() gansi.StyleConfig {
 	noColor := strptr("")
 	s := styles.DarkStyleConfig
-	// s.H1.BackgroundColor = noColor
-	// s.H1.Prefix = "# "
-	// s.H1.Suffix = ""
-	// s.H1.Color = strptr("39")
-	// s.Document.StylePrimitive.Color = noColor
-	// s.CodeBlock.Chroma.Text.Color = noColor
-	// s.CodeBlock.Chroma.Name.Color = noColor
 	// This fixes an issue with the default style config. For example
 	// highlighting empty spaces with red in Dockerfile type.
 	s.CodeBlock.Chroma.Error.BackgroundColor = noColor
