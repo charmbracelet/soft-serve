7e944a21a690306ad942ae825a09092090d687b8
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-07-03 11:49:41 -0400

    fix(ui): remove red background for empty spaces in code blocks

--- pkg/ui/common/style.go
+++ pkg/ui/common/style.go
@@ -15,7 +15,7 @@ func strptr(s string) *string {
 
 // StyleConfig returns the default Glamour style configuration.
 func StyleConfig() gansi.StyleConfig {
-	// noColor := strptr("")
+	noColor := strptr("")
 	s := styles.DarkStyleConfig
 	// s.H1.BackgroundColor = noColor
 	// s.H1.Prefix = "# "
@@ -24,9 +24,9 @@ func StyleConfig() gansi.StyleConfig {
 	// s.Document.StylePrimitive.Color = noColor
 	// s.CodeBlock.Chroma.Text.Color = noColor
 	// s.CodeBlock.Chroma.Name.Color = noColor
-	// // This fixes an issue with the default style config. For example
-	// // highlighting empty spaces with red in Dockerfile type.
-	// s.CodeBlock.Chroma.Error.BackgroundColor = noColor
+	// This fixes an issue with the default style config. For example
+	// highlighting empty spaces with red in Dockerfile type.
+	s.CodeBlock.Chroma.Error.BackgroundColor = noColor
 	return s
 }
 
